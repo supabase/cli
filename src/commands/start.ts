613e9601ffe21d0c9b425d8885ae6bf6b0d8b0c8
Bobbie Soedirgo <bobbie@soedirgo.dev>
2021-04-01 19:14:54 +0800

    fix: catch more runtime errors

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -3,23 +3,37 @@ import { GluegunToolbox } from 'gluegun'
 export default {
   name: 'start',
   run: async ({
+    filesystem: { exists },
     print: {
       colors: { highlight },
+      error,
       spin,
     },
     system: { run, which },
   }: GluegunToolbox) => {
-    const spinner = spin('Starting local Supabase...')
-
-    const dockerCompose = which('docker-compose')
-    if (!dockerCompose) {
-      spinner.fail(`Cannot find ${highlight('docker-compose')} executable in PATH.`)
+    if (!exists('.supabase')) {
+      error(
+        `Cannot find ${highlight(
+          '.supabase'
+        )} in the current directory. Perhaps you meant to run ${highlight('supabase init')} first?`
+      )
       process.exit(1)
     }
 
+    const dockerCompose = which('docker-compose')
+    if (!dockerCompose) {
+      error(`Cannot find ${highlight('docker-compose')} executable in PATH.`)
+      process.exit(1)
+    }
+
+    const spinner = spin('Starting local Supabase...')
+
     await run(
       'docker-compose --file .supabase/docker/docker-compose.yml --project-name supabase up --detach'
-    )
+    ).catch(() => {
+      spinner.fail('Error running docker-compose.')
+      process.exit(1)
+    })
 
     spinner.succeed('Started local Supabase.')
   },
