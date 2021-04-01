8c200aaf9b7d2f8a5b11e545b9dd917071408886
Bobbie Soedirgo <bobbie@soedirgo.dev>
2021-04-01 19:02:36 +0800

    fix: remove gluegun's unhandledRejection handler
    So when you Ctrl+C it doesn't show node backtraces, like a sane person.

--- src/cli.ts
+++ src/cli.ts
@@ -2,4 +2,9 @@
 
 import { build } from 'gluegun'
 
+process.removeAllListeners('unhandledRejection')
+process.on('unhandledRejection', () => {
+  process.exit(1)
+})
+
 build('supabase').src(__dirname).create().run()
