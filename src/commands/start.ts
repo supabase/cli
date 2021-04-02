f521000504fd311bf24b1cd9cd65ccc3788b261e
Bobbie Soedirgo <bobbie@soedirgo.dev>
2021-04-03 01:36:04 +0800

    fix: build & create container on init

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -29,7 +29,7 @@ export default {
     const spinner = spin('Starting local Supabase...')
 
     await run(
-      'docker-compose --file .supabase/docker/docker-compose.yml --project-name supabase up --detach'
+      'docker-compose --file .supabase/docker/docker-compose.yml --project-name supabase start'
     ).catch(() => {
       spinner.fail('Error running docker-compose.')
       process.exit(1)
