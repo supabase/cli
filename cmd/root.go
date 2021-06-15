51c9d385b8ef483c0764595a3c4046f85dd43669
Bobbie Soedirgo <bobbie@soedirgo.dev>
2021-06-15 19:58:16 +0800

    feat: set up barebones CLI

---
+++ cmd/root.go
@@ -0,0 +1,62 @@
+package cmd
+
+import (
+	// "fmt"
+	// "os"
+
+	// homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	// "github.com/spf13/viper"
+)
+
+// var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "supabase",
+	Short: "TODO",
+	Long: `TODO`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	// cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cli.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+// func initConfig() {
+// 	if cfgFile != "" {
+// 		// Use config file from the flag.
+// 		viper.SetConfigFile(cfgFile)
+// 	} else {
+// 		// Find home directory.
+// 		home, err := homedir.Dir()
+// 		cobra.CheckErr(err)
+// 
+// 		// Search config in home directory with name ".cli" (without extension).
+// 		viper.AddConfigPath(home)
+// 		viper.SetConfigName(".cli")
+// 	}
+// 
+// 	viper.AutomaticEnv() // read in environment variables that match
+// 
+// 	// If a config file is found, read it in.
+// 	if err := viper.ReadInConfig(); err == nil {
+// 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+// 	}
+// }
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/supabase/cli
+
+go 1.16
+
+require (
+	github.com/containerd/containerd v1.5.2 // indirect
+	github.com/docker/docker v20.10.7+incompatible
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/spf13/cobra v1.1.3
+)
