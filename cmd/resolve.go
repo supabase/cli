c6c86cd8ad58617b3d662612ff388ae30a079754
Bobbie Soedirgo <bobbie@soedirgo.dev>
2021-09-03 04:34:33 +0800

    fix: resolve command

--- cmd/resolve.go
+++ cmd/resolve.go
@@ -8,17 +8,17 @@ import (
 )
 
 var (
-	appliedTimestamp    *string
-	rolledBackTimestamp *string
+	appliedTimestamp    string
+	rolledBackTimestamp string
 
 	resolveCmd = &cobra.Command{
 		Use:   "resolve",
 		Short: "Resolve issues with migration history on the deploy database.",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if appliedTimestamp != nil {
-				return resolve.ResolveApplied(*appliedTimestamp)
-			} else if rolledBackTimestamp != nil {
-				return resolve.ResolveRolledBack(*rolledBackTimestamp)
+			if appliedTimestamp != "" {
+				return resolve.ResolveApplied(appliedTimestamp)
+			} else if rolledBackTimestamp != "" {
+				return resolve.ResolveRolledBack(rolledBackTimestamp)
 			} else {
 				return errors.New("Must set either --applied or --rolled-back.")
 			}
@@ -27,8 +27,8 @@ var (
 )
 
 func init() {
-	resolveCmd.Flags().StringVar(appliedTimestamp, "applied", "", "Migration timestamp to be recorded as applied.")
-	resolveCmd.Flags().StringVar(rolledBackTimestamp, "rolled-back", "", "Migration timestamp to be recorded as rolled back.")
+	resolveCmd.Flags().StringVar(&appliedTimestamp, "applied", "", "Migration timestamp to be recorded as applied.")
+	resolveCmd.Flags().StringVar(&rolledBackTimestamp, "rolled-back", "", "Migration timestamp to be recorded as rolled back.")
 
 	rootCmd.AddCommand(resolveCmd)
 }
