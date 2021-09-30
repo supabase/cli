8489a08995139cf5029dbf9fcf221870c7dece11
Bobbie Soedirgo <bobbie@soedirgo.dev>
2021-10-01 06:33:17 +0800

    fix: accept deploy db URL from stdin

--- cmd/link.go
+++ cmd/link.go
@@ -1,26 +1,38 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/spf13/cobra"
 	"github.com/supabase/cli/internal/link"
 )
 
 var (
-	deployDbUrl string
+	useUrl bool
 
 	linkCmd = &cobra.Command{
 		Use:   "link",
 		Short: "Link the current project to a remote deploy database.",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return link.Link(deployDbUrl)
+			if useUrl {
+				var deployDbUrl string
+				fmt.Scanln(&deployDbUrl)
+				if len(deployDbUrl) == 0 {
+					return errors.New("Error on `supabase link`: URL is empty.")
+				}
+
+				return link.Link(deployDbUrl)
+			}
+
+			return errors.New("Use `--url` to pass the deploy database URL to link to.")
 		},
 	}
 )
 
 func init() {
 	linkCmd.Flags().
-		StringVar(&deployDbUrl, "url", "", "Postgres connection string of the deploy database.")
-	cobra.CheckErr(linkCmd.MarkFlagRequired("url"))
+		BoolVar(&useUrl, "url", false, "Accept Postgres connection string of the deploy database from standard input.")
 
 	rootCmd.AddCommand(linkCmd)
 }
