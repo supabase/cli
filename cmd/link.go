3ae7227c4b03c3e3539d3c2535f903bdb0855e01
Bobbie Soedirgo <bobbie@soedirgo.dev>
2021-10-26 20:48:32 +0800

    fix(link): take url as string arg

--- cmd/link.go
+++ cmd/link.go
@@ -1,38 +1,26 @@
 package cmd
 
 import (
-	"errors"
-	"fmt"
-
 	"github.com/spf13/cobra"
 	"github.com/supabase/cli/internal/link"
 )
 
 var (
-	useUrl bool
+	deployDbUrl string
 
 	linkCmd = &cobra.Command{
 		Use:   "link",
 		Short: "Link the current project to a remote deploy database.",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if useUrl {
-				var deployDbUrl string
-				fmt.Scanln(&deployDbUrl)
-				if len(deployDbUrl) == 0 {
-					return errors.New("Error on `supabase link`: URL is empty.")
-				}
-
-				return link.Link(deployDbUrl)
-			}
-
-			return errors.New("Use `--url` to pass the deploy database URL to link to.")
+			return link.Link(deployDbUrl)
 		},
 	}
 )
 
 func init() {
 	linkCmd.Flags().
-		BoolVar(&useUrl, "url", false, "Accept Postgres connection string of the deploy database from standard input.")
+		StringVar(&deployDbUrl, "url", "", "Postgres connection string of the deploy database.")
+	cobra.CheckErr(linkCmd.MarkFlagRequired("url"))
 
 	rootCmd.AddCommand(linkCmd)
 }
