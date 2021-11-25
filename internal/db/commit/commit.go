3df51e36390792906d891e198d49f304f3265d0b
Bobbie Soedirgo <bobbie@soedirgo.dev>
2021-11-26 04:14:53 +0800

    feat: filter out noise from diff output
    * only diff functions, tables, types, (materialized) views
    * ignore diffs from auth, extensions, pgbouncer, realtime, storage,
    supabase_realtime
    * ignore extension objects

--- internal/db/commit/commit.go
+++ internal/db/commit/commit.go
@@ -197,7 +197,7 @@ EOSQL
 	// 2. Diff it (target) with local db (source), write it as a new migration.
 	{
 		out, err := utils.DockerExec(ctx, utils.DifferId, []string{
-			"sh", "-c", "/venv/bin/python3 -u cli.py " +
+			"sh", "-c", "/venv/bin/python3 -u cli.py --json-diff " +
 				"'postgres://postgres:postgres@" + utils.DbId + ":5432/" + currBranch + "' " +
 				"'postgres://postgres:postgres@" + utils.DbId + ":5432/" + utils.ShadowDbName + "'",
 		})
