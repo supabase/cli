2806bc88b6aa0e7e237be8e219c8d1ea242191c0
Wen Bo Xie <5532241+w3b6x9@users.noreply.github.com>
2021-12-23 02:02:31 -0800

    fix: update Realtime configs for RLS (#115)
    * feat: update Realtime configs for RLS
    
    * fix(realtime): migrate on start
    
    Co-authored-by: Bobbie Soedirgo <bobbie@soedirgo.dev>

--- internal/start/start.go
+++ internal/start/start.go
@@ -692,15 +692,23 @@ EOSQL
 		Image: utils.RealtimeImage,
 		Env: []string{
 			// connect to db directly instead of pgbouncer, since realtime doesn't work with pgbouncer for some reason
+			"PORT=4000",
 			"DB_HOST=" + utils.DbId,
 			"DB_PORT=5432",
 			"DB_USER=postgres",
 			"DB_PASSWORD=postgres",
 			"DB_NAME=" + currBranch,
+			"DB_SSL=false",
 			"SLOT_NAME=supabase_realtime",
-			"PORT=4000",
-			"SECURE_CHANNELS=true",
+			"TEMPORARY_SLOT=true",
 			"JWT_SECRET=super-secret-jwt-token-with-at-least-32-characters-long",
+			"SECURE_CHANNELS=true",
+			"REPLICATION_MODE=RLS",
+			"REPLICATION_POLL_INTERVAL=100",
+		},
+		Cmd: []string{
+			"sh", "-c",
+			"./prod/rel/realtime/bin/realtime eval Realtime.Release.migrate && ./prod/rel/realtime/bin/realtime start",
 		},
 	}, &container.HostConfig{NetworkMode: container.NetworkMode(utils.NetId)}); err != nil {
 		return err
@@ -851,15 +859,19 @@ EOSQL
 			Image: utils.RealtimeImage,
 			Env: []string{
 				// connect to db directly instead of pgbouncer, since realtime doesn't work with pgbouncer for some reason
+				"PORT=4000",
 				"DB_HOST=" + utils.DbId,
 				"DB_PORT=5432",
 				"DB_USER=postgres",
 				"DB_PASSWORD=postgres",
 				"DB_NAME=" + currBranch,
+				"DB_SSL=false",
 				"SLOT_NAME=supabase_realtime",
-				"PORT=4000",
-				"SECURE_CHANNELS=true",
+				"TEMPORARY_SLOT=true",
 				"JWT_SECRET=super-secret-jwt-token-with-at-least-32-characters-long",
+				"SECURE_CHANNELS=true",
+				"REPLICATION_MODE=RLS",
+				"REPLICATION_POLL_INTERVAL=100",
 			},
 		}, &container.HostConfig{NetworkMode: container.NetworkMode(utils.NetId)}); err != nil {
 			return err
