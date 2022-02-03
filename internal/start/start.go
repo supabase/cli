599a86e78175448fd14a8a59c639034203b5b42d
Marek Gilbert <gil@fooplanet.com>
2022-02-03 11:16:31 -0800

    Use a fake implementation of Program when not connected to a terminal

--- internal/start/start.go
+++ internal/start/start.go
@@ -47,7 +47,7 @@ func Run() error {
 	s := spinner.NewModel()
 	s.Spinner = spinner.Dot
 	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
-	p := tea.NewProgram(model{spinner: s})
+	p := utils.NewProgram(model{spinner: s})
 
 	errCh := make(chan error, 1)
 	go func() {
