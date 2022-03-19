92d597d0f0d94270e09cf468addd7631316e67e1
Bobbie Soedirgo <bobbie@soedirgo.dev>
2022-03-19 14:13:09 +0100

    feat: `supabase login` & `supabase link`

---
+++ cmd/link.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/supabase/cli/internal/link"
+)
+
+var (
+	linkCmd = &cobra.Command{
+		Use:   "link",
+		Short: "Link to a Supabase project.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			projectRef, err := cmd.Flags().GetString("ref")
+			if err != nil {
+				return err
+			}
+
+			return link.Run(projectRef)
+		},
+	}
+)
+
+func init() {
+	linkCmd.Flags().String("ref", "", "Project ref of the Supabase project")
+	linkCmd.MarkFlagRequired("ref")
+	rootCmd.AddCommand(linkCmd)
+}
