08a33baa33382087c7e17009478428fcb8466284
Thor 雷神 Schaeff <5748289+thorwebdev@users.noreply.github.com>
2022-03-28 09:47:18 +0200

    chore: improve functions curl example.

--- internal/functions/new/new.go
+++ internal/functions/new/new.go
@@ -53,9 +53,9 @@ serve(async (req) => {
 })
 
 // To invoke:
-// curl --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs'
-//   --header 'Content-Type: application/json'
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs' \
+//   --header 'Content-Type: application/json' \
 //   --data '{"name":"Functions"}'
 `),
 			0644,
