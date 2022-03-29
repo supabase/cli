665e54fcff7abae4156a41548dfdf7fbbf6c5e33
Bobbie Soedirgo <bobbie@soedirgo.dev>
2022-03-29 21:04:39 +0200

    docs: remove tour
    This is now on https://supabase.com/docs/guides/local-development

--- examples/tour/index.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require('@supabase/supabase-js')
-const supabase = createClient(
-  '<your_API_URL>',
-  '<your_anon_key>'
-)
-supabase
-  .from('boarders')
-  .select()
-  .then(({ data }) => console.log(data))
