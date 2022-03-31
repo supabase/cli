3407985779def92ba0d8197221752e1ef7f8130d
awalias <antwilson@hotmail.co.uk>
2022-03-31 02:43:04 +0200

    chore: adds project ref optional arg to functions deploy

--- cmd/functions.go
+++ cmd/functions.go
@@ -26,9 +26,13 @@ var (
 	functionsDeployCmd = &cobra.Command{
 		Use:   "deploy <Function name>",
 		Short: "Deploy a Function to the linked Supabase project.",
-		Args:  cobra.ExactArgs(1),
+		Args:  cobra.RangeArgs(1, 2),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return deploy.Run(args[0])
+			projectRef, err := cmd.Flags().GetString("project-ref")
+			if err != nil {
+				return err
+			}
+			return deploy.Run(args[0], projectRef)
 		},
 	}
 
@@ -58,6 +62,7 @@ var (
 
 func init() {
 	functionsServeCmd.Flags().String("env-file", "", "Path to an env file to be populated to the Function environment")
+	functionsDeployCmd.Flags().String("project-ref", "", "Project ref of the Supabase project")
 	functionsCmd.AddCommand(functionsDeleteCmd)
 	functionsCmd.AddCommand(functionsDeployCmd)
 	functionsCmd.AddCommand(functionsNewCmd)
