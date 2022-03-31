a864e7ff1e67c7c3e486208f39e1e1f62fed0434
Bobbie Soedirgo <bobbie@soedirgo.dev>
2022-03-31 03:19:08 +0200

    fix(functions/deploy): ask creds interactively

--- cmd/init.go
+++ cmd/init.go
@@ -1,15 +1,23 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 	_init "github.com/supabase/cli/internal/init"
+	"github.com/supabase/cli/internal/utils"
 )
 
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize a project to use Supabase CLI.",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return _init.Run()
+		if err := _init.Run(); err != nil {
+			return err
+		}
+
+		fmt.Println("Finished " + utils.Aqua("supabase init") + ".")
+		return nil
 	},
 }
 
