bc9cf199b5351fcb8f2cbb97439b4ed39b5458ea
Bobbie Soedirgo <bobbie@soedirgo.dev>
2022-06-23 20:18:30 +0800

    feat(projects/create): add command

--- cmd/projects.go
+++ cmd/projects.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/supabase/cli/internal/projects/create"
 	"github.com/supabase/cli/internal/projects/list"
 )
 
@@ -9,6 +10,34 @@ var (
 	projectsCmd = &cobra.Command{
 		Use:   "projects",
 		Short: "Manage Supabase projects",
+	}
+
+	projectsCreateCmd = &cobra.Command{
+		Use:   "create",
+		Short: "Create a new project.",
+		Args:  cobra.ExactArgs(1),
+		Example: `supabase projects create my-project --org-id 12345 --db-password ******** --region us-east-1`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			name := args[0]
+			orgId, err := cmd.Flags().GetUint("org-id")
+			if err != nil {
+				return err
+			}
+			dbPassword, err := cmd.Flags().GetString("db-password")
+			if err != nil {
+				return err
+			}
+			region, err := cmd.Flags().GetString("region")
+			if err != nil {
+				return err
+			}
+			plan, err := cmd.Flags().GetString("plan")
+			if err != nil {
+				return err
+			}
+
+			return create.Run(name, orgId, dbPassword, region, plan)
+		},
 	}
 
 	projectsListCmd = &cobra.Command{
@@ -21,6 +50,21 @@ var (
 )
 
 func init() {
+	// TODO: Make these optional once we implement prompting missing flags.
+	projectsCreateCmd.Flags().Uint("org-id", 0, "Organization ID to create the project in.")
+	if err := projectsCreateCmd.MarkFlagRequired("org-id"); err != nil {
+		panic(err)
+	}
+	projectsCreateCmd.Flags().String("db-password", "", "Database password of the project.")
+	if err := projectsCreateCmd.MarkFlagRequired("db-password"); err != nil {
+		panic(err)
+	}
+	projectsCreateCmd.Flags().String("region", "", "Select a region close to you for the best performance.")
+	if err := projectsCreateCmd.MarkFlagRequired("region"); err != nil {
+		panic(err)
+	}
+	projectsCreateCmd.Flags().String("plan", "free", `Select a plan that suits your needs. Can be "free" or "pro".`)
+	projectsCmd.AddCommand(projectsCreateCmd)
 	projectsCmd.AddCommand(projectsListCmd)
 	rootCmd.AddCommand(projectsCmd)
 }
---
+++ internal/projects/create/create.go
@@ -0,0 +1,78 @@
+package create
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/supabase/cli/internal/utils"
+)
+
+func Run(name string, orgId uint, dbPassword string, region string, plan string) error {
+	accessToken, err := utils.LoadAccessToken()
+	if err != nil {
+		return err
+	}
+
+	// TODO: Prompt missing args.
+	{}
+
+	// POST request, check errors
+	var project struct {
+		Id     uint   `json:"id"`
+		Ref    string `json:"ref"`
+		Name   string `json:"name"`
+		OrgId  uint   `json:"organization_id"`
+		Region string `json:"region"`
+	}
+	{
+		jsonBytes, err := json.Marshal(map[string]interface{}{
+			"organization_id": orgId,
+			"name":            name,
+			"db_pass":         dbPassword,
+			"region":          region,
+			"plan":            plan,
+		})
+		if err != nil {
+			return err
+		}
+
+		req, err := http.NewRequest("POST", "https://api.supabase.io/v1/projects", bytes.NewReader(jsonBytes))
+		if err != nil {
+			return err
+		}
+		req.Header.Add("Authorization", "Bearer "+string(accessToken))
+		req.Header.Add("Content-Type", "application/json")
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				return fmt.Errorf("Unexpected error creating project: %w", err)
+			}
+
+			return errors.New("Unexpected error creating project: " + string(body))
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		if err := json.Unmarshal(body, &project); err != nil {
+			return fmt.Errorf("Failed to create a new project: %w", err)
+		}
+	}
+
+	// TODO: Poll until PostgREST is reachable.
+	{}
+
+	fmt.Printf(`Created a new project %s at %s\n`, utils.Aqua(project.Name), utils.Aqua("https://app.supabase.com/project/"+project.Ref))
+	return nil
+}
