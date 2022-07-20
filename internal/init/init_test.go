f8a401d7c0f560bcd5a9e71b9e5561c7cb22d747
Qiao Han <qiao@supabase.io>
2022-07-20 16:59:39 +0800

    Improve init test coverage

--- internal/init/init_test.go
+++ internal/init/init_test.go
@@ -1,29 +1,64 @@
 package init
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/supabase/cli/internal/utils"
 )
+
+func gitIgnorePath(t *testing.T) string {
+	root, err := utils.GetGitRoot()
+	assert.NoError(t, err)
+	return filepath.Join(*root, ".gitignore")
+}
 
 func TestInitCommand(t *testing.T) {
 	t.Run("creates config file", func(t *testing.T) {
+		// Setup in-memory fs
 		fsys := &afero.MemMapFs{}
+		// Run test
 		assert.NoError(t, Run(fsys))
-		// TODO: verify .gitignore
+		// Validate generated config.toml
 		exists, err := afero.Exists(fsys, "supabase/config.toml")
+		assert.NoError(t, err)
+		assert.True(t, exists)
+		// Validate generated .gitignore
+		exists, err = afero.Exists(fsys, gitIgnorePath(t))
 		assert.NoError(t, err)
 		assert.True(t, exists)
 	})
 
-	t.Run("errors when config file exists", func(t *testing.T) {
-		// Test setup
+	t.Run("throws error when config file exists", func(t *testing.T) {
+		// Setup in-memory fs
 		fsys := &afero.MemMapFs{}
 		_, err := fsys.Create("supabase/config.toml")
 		require.NoError(t, err)
-		// Actual test
+		// Run test
 		assert.Error(t, Run(fsys))
+	})
+
+	t.Run("throws error on failure to create directory", func(t *testing.T) {
+		// Setup read-only fs
+		fsys := afero.NewReadOnlyFs(afero.NewMemMapFs())
+		// Run test
+		assert.Error(t, Run(fsys))
+	})
+
+	t.Run("appends to git ignore", func(t *testing.T) {
+		// Setup in-memory fs
+		fsys := &afero.MemMapFs{}
+		path := gitIgnorePath(t)
+		_, err := fsys.Create(path)
+		require.NoError(t, err)
+		// Run test
+		assert.NoError(t, Run(fsys))
+		// Validate file contents
+		content, err := afero.ReadFile(fsys, path)
+		assert.NoError(t, err)
+		assert.Equal(t, initGitignore, content[1:])
 	})
 }
