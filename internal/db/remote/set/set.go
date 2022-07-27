ff9b966559b32031250966c0b05959c5a1767811
Qiao Han <qiao@supabase.io>
2022-07-27 17:54:14 +0800

    test: remote set command

--- internal/db/remote/set/set.go
+++ internal/db/remote/set/set.go
@@ -6,13 +6,29 @@ import (
 	"fmt"
 	u "net/url"
 	"path/filepath"
+	"strconv"
 
-	pgx "github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4"
 	"github.com/spf13/afero"
 	"github.com/supabase/cli/internal/utils"
 )
 
-func Run(url string, fsys afero.Fs) error {
+const (
+	CHECK_MIGRATION_EXISTS = "SELECT 1 FROM supabase_migrations.schema_migrations LIMIT 1"
+	LIST_MIGRATION_VERSION = "SELECT version FROM supabase_migrations.schema_migrations ORDER BY version"
+	CREATE_MIGRATION_TABLE = `CREATE SCHEMA IF NOT EXISTS supabase_migrations;
+CREATE TABLE supabase_migrations.schema_migrations (version text NOT NULL PRIMARY KEY);
+`
+)
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Run(url string, fsys afero.Fs, options ...func(*pgx.ConnConfig)) error {
 	// Sanity checks.
 	{
 		if err := utils.LoadConfigFS(fsys); err != nil {
@@ -26,20 +42,39 @@ func Run(url string, fsys afero.Fs) error {
 	}
 	url = "postgresql://postgres:" + u.QueryEscape(matches[1]) + "@" + u.QueryEscape(matches[2]) + matches[3] + "/postgres"
 
-	ctx := context.Background()
-
-	conn, err := pgx.Connect(ctx, url)
+	config, err := pgx.ParseConfig(url)
 	if err != nil {
 		return err
 	}
-	defer conn.Close(context.Background())
 
-	// 1. Assert db.major_version is compatible.
-	var dbMajorVersion uint
-	if err := conn.QueryRow(ctx, "SELECT current_setting('server_version_num')::int8 / 10000").Scan(&dbMajorVersion); err != nil {
+	// Simple protocol is preferred over pgx default Parse -> Bind flow because
+	//   1. Using a single command for each query reduces RTT over an Internet connection.
+	//   2. Performance gains from using the alternate binary protocol is negligible because
+	//      we are only selecting from migrations table. Large reads are handled by PostgREST.
+	//   3. Any prepared statements are cleared server side upon closing the TCP connection.
+	//      Since CLI workloads are one-off scripts, we don't use connection pooling and hence
+	//      don't benefit from per connection server side cache.
+	config.PreferSimpleProtocol = true
+	for _, op := range options {
+		op(config)
+	}
+
+	ctx := context.Background()
+	conn, err := pgx.ConnectConfig(ctx, config)
+	if err != nil {
 		return err
 	}
-	if dbMajorVersion != utils.Config.Db.MajorVersion {
+	defer conn.Close(ctx)
+
+	// 1. Assert db.major_version is compatible.
+	serverVersion := conn.PgConn().ParameterStatus("server_version")
+	// Safe to assume that supported Postgres version is 10.0 <= n < 100.0
+	dbMajorVersion, err := strconv.ParseUint(serverVersion[:min(2, len(serverVersion))], 10, 7)
+	if err != nil {
+		return err
+	}
+
+	if dbMajorVersion != uint64(utils.Config.Db.MajorVersion) {
 		return fmt.Errorf(
 			"Remote database Postgres version %[1]d is incompatible with %[3]s %[2]d. If you are setting up a fresh Supabase CLI project, try changing %[3]s in %[4]s to %[1]d.",
 			dbMajorVersion,
@@ -52,20 +87,15 @@ func Run(url string, fsys afero.Fs) error {
 	// 2. Setup & validate `schema_migrations`.
 
 	// If `schema_migrations` doesn't exist on the remote database, create it.
-	if _, err := conn.Exec(ctx, "SELECT 1 FROM supabase_migrations.schema_migrations"); err != nil {
-		if _, err := conn.Exec(
-			ctx,
-			`CREATE SCHEMA IF NOT EXISTS supabase_migrations;
-CREATE TABLE supabase_migrations.schema_migrations (version text NOT NULL PRIMARY KEY);
-`,
-		); err != nil {
+	if _, err := conn.Exec(ctx, CHECK_MIGRATION_EXISTS); err != nil {
+		if _, err := conn.Exec(ctx, CREATE_MIGRATION_TABLE); err != nil {
 			return err
 		}
 	}
 
 	// If `schema_migrations` is not a "prefix" of list of migrations in repo, fail &
 	// warn user.
-	rows, err := conn.Query(ctx, "SELECT version FROM supabase_migrations.schema_migrations ORDER BY version")
+	rows, err := conn.Query(ctx, LIST_MIGRATION_VERSION)
 	if err != nil {
 		return err
 	} else {
@@ -78,7 +108,7 @@ CREATE TABLE supabase_migrations.schema_migrations (version text NOT NULL PRIMAR
 			remoteMigrations = append(remoteMigrations, version)
 		}
 
-		if err := utils.MkdirIfNotExist("supabase/migrations"); err != nil {
+		if err := utils.MkdirIfNotExistFS(fsys, "supabase/migrations"); err != nil {
 			return err
 		}
 		localMigrations, err := afero.ReadDir(fsys, "supabase/migrations")
@@ -94,8 +124,8 @@ CREATE TABLE supabase_migrations.schema_migrations (version text NOT NULL PRIMAR
 3. Run ` + utils.Aqua("supabase db remote commit") + ".")
 			}
 
-			localTimestamp := utils.MigrateFilePattern.FindStringSubmatch(localMigrations[i].Name())[1]
-			if localTimestamp == remoteTimestamp {
+			localTimestamp := utils.MigrateFilePattern.FindStringSubmatch(localMigrations[i].Name())
+			if len(localTimestamp) > 1 && localTimestamp[1] == remoteTimestamp {
 				continue
 			}
 
