a38ff4069b62dab573657037ae2dac1f04d67a1f
Qiao Han <qiao@supabase.io>
2022-08-08 10:58:50 +0800

    chore: add more docker utils

--- internal/utils/docker.go
+++ internal/utils/docker.go
@@ -4,6 +4,7 @@ import (
 	"archive/tar"
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -12,6 +13,9 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/jsonmessage"
+	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/spf13/viper"
 )
 
 var Docker = func() *client.Client {
@@ -135,4 +139,115 @@ func DockerAddFile(ctx context.Context, container string, fileName string, conte
 		return fmt.Errorf("failed to copy file: %v", err)
 	}
 	return nil
+}
+
+func DockerPullImageIfNotCached(ctx context.Context, imageName string) error {
+	imageUrl := "docker.io/" + imageName
+	if _, _, err := Docker.ImageInspectWithRaw(ctx, imageUrl); err == nil {
+		return nil
+	}
+	out, err := Docker.ImagePull(ctx, imageUrl, types.ImagePullOptions{})
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	fmt.Fprintln(os.Stderr, "Pulling docker image...")
+	if viper.GetBool("DEBUG") {
+		return jsonmessage.DisplayJSONMessagesStream(out, os.Stderr, os.Stderr.Fd(), true, nil)
+	}
+	_, err = io.Copy(io.Discard, out)
+	return err
+}
+
+// Runs a container image exactly once, returning stdout and throwing error on non-zero exit code.
+func DockerRunOnce(ctx context.Context, image string, env []string, cmd []string) (string, error) {
+	// Pull container image
+	if err := DockerPullImageIfNotCached(ctx, image); err != nil {
+		return "", err
+	}
+	// Create container from image
+	resp, err := Docker.ContainerCreate(ctx, &container.Config{
+		Image: image,
+		Env:   env,
+		Cmd:   cmd,
+		Labels: map[string]string{
+			"com.supabase.cli.project":   Config.ProjectId,
+			"com.docker.compose.project": Config.ProjectId,
+		},
+	}, &container.HostConfig{
+		NetworkMode: container.NetworkMode(NetId),
+		AutoRemove:  true,
+	}, nil, nil, "")
+	if err != nil {
+		return "", err
+	}
+	// Run container in background and propagate cancellation
+	if err := Docker.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return "", err
+	}
+	go func() {
+		<-ctx.Done()
+		if ctx.Err() != nil {
+			if err := Docker.ContainerStop(context.Background(), resp.ID, nil); err != nil {
+				fmt.Fprintln(os.Stderr, "Failed to stop container:", resp.ID, err)
+			}
+		}
+	}()
+	// Stream logs
+	logs, err := Docker.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{
+		ShowStdout: true,
+		ShowStderr: viper.GetBool("DEBUG"),
+		Follow:     true,
+	})
+	if err != nil {
+		return "", err
+	}
+	defer logs.Close()
+	var out bytes.Buffer
+	if _, err := stdcopy.StdCopy(&out, os.Stderr, logs); err != nil {
+		return "", err
+	}
+	// Check exit code
+	iresp, err := Docker.ContainerInspect(ctx, resp.ID)
+	if err != nil {
+		return "", err
+	}
+	if iresp.State.ExitCode > 0 {
+		return "", errors.New("error running container")
+	}
+	return out.String(), nil
+}
+
+// Exec a command once inside a container, returning stdout and throwing error on non-zero exit code.
+func DockerExecOnce(ctx context.Context, container string, env []string, cmd []string) (string, error) {
+	// Reset shadow database
+	exec, err := Docker.ContainerExecCreate(ctx, container, types.ExecConfig{
+		Env:          env,
+		Cmd:          cmd,
+		AttachStderr: viper.GetBool("DEBUG"),
+		AttachStdout: true,
+	})
+	if err != nil {
+		return "", err
+	}
+	// Read exec output
+	resp, err := Docker.ContainerExecAttach(ctx, exec.ID, types.ExecStartCheck{})
+	if err != nil {
+		return "", err
+	}
+	defer resp.Close()
+	// Capture error details
+	var out bytes.Buffer
+	if _, err := stdcopy.StdCopy(&out, os.Stderr, resp.Reader); err != nil {
+		return "", err
+	}
+	// Get the exit code
+	iresp, err := Docker.ContainerExecInspect(ctx, exec.ID)
+	if err != nil {
+		return "", err
+	}
+	if iresp.ExitCode > 0 {
+		return "", errors.New("error executing command")
+	}
+	return out.String(), nil
 }
---
+++ internal/utils/docker_test.go
@@ -0,0 +1,366 @@
+package utils
+
+import (
+	"bytes"
+	"context"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/h2non/gock.v1"
+)
+
+const (
+	version     = "1.41"
+	containerId = "test-container"
+	imageId     = "test-image"
+)
+
+func TestPullImage(t *testing.T) {
+	t.Run("pulls image if missing", func(t *testing.T) {
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(Docker))
+		defer gock.Off()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images/docker.io/" + imageId + "/json").
+			Reply(http.StatusNotFound)
+		gock.New("http:///var/run/docker.sock").
+			Post("/v"+version+"/images/create").
+			MatchParam("fromImage", imageId).
+			MatchParam("tag", "latest").
+			Reply(http.StatusAccepted)
+		// Run test
+		assert.NoError(t, DockerPullImageIfNotCached(context.Background(), imageId))
+		assert.False(t, gock.HasUnmatchedRequest())
+	})
+
+	t.Run("does nothing if image exists", func(t *testing.T) {
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(Docker))
+		defer gock.Off()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images/docker.io/" + imageId + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ImageInspect{})
+		// Run test
+		assert.NoError(t, DockerPullImageIfNotCached(context.Background(), imageId))
+		assert.False(t, gock.HasUnmatchedRequest())
+	})
+
+	t.Run("throws error on failure to pull image", func(t *testing.T) {
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(Docker))
+		defer gock.Off()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images/docker.io/" + imageId + "/json").
+			Reply(http.StatusNotFound)
+		gock.New("http:///var/run/docker.sock").
+			Post("/v"+version+"/images/create").
+			MatchParam("fromImage", imageId).
+			MatchParam("tag", "latest").
+			Reply(http.StatusServiceUnavailable)
+		// Run test
+		assert.Error(t, DockerPullImageIfNotCached(context.Background(), imageId))
+		assert.False(t, gock.HasUnmatchedRequest())
+	})
+}
+
+func TestRunOnce(t *testing.T) {
+	t.Run("runs once in container", func(t *testing.T) {
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(Docker))
+		defer gock.Off()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images/docker.io/" + imageId + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ImageInspect{})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/create").
+			Reply(http.StatusOK).
+			JSON(container.ContainerCreateCreatedBody{ID: containerId})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/" + containerId + "/start").
+			Reply(http.StatusAccepted)
+		// Setup docker style logs
+		var body bytes.Buffer
+		writer := stdcopy.NewStdWriter(&body, stdcopy.Stdout)
+		_, err := writer.Write([]byte("hello world"))
+		require.NoError(t, err)
+		gock.New("http:///var/run/docker.sock").
+			Get("/v"+version+"/containers/"+containerId+"/logs").
+			Reply(http.StatusOK).
+			SetHeader("Content-Type", "application/vnd.docker.raw-stream").
+			Body(&body)
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/containers/" + containerId + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ContainerJSONBase{State: &types.ContainerState{ExitCode: 0}})
+		// Run test
+		out, err := DockerRunOnce(context.Background(), imageId, nil, nil)
+		assert.NoError(t, err)
+		// Validate output
+		assert.Equal(t, "hello world", out)
+		assert.False(t, gock.HasUnmatchedRequest())
+	})
+
+	t.Run("throws error on container create", func(t *testing.T) {
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(Docker))
+		defer gock.Off()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images/docker.io/" + imageId + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ImageInspect{})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/create").
+			Reply(http.StatusServiceUnavailable)
+		// Run test
+		_, err := DockerRunOnce(context.Background(), imageId, nil, nil)
+		assert.Error(t, err)
+		assert.False(t, gock.HasUnmatchedRequest())
+	})
+
+	t.Run("throws error on container start", func(t *testing.T) {
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(Docker))
+		defer gock.Off()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images/docker.io/" + imageId + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ImageInspect{})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/create").
+			Reply(http.StatusOK).
+			JSON(container.ContainerCreateCreatedBody{ID: containerId})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/" + containerId + "/start").
+			Reply(http.StatusServiceUnavailable)
+		// Run test
+		_, err := DockerRunOnce(context.Background(), imageId, nil, nil)
+		assert.Error(t, err)
+		assert.False(t, gock.HasUnmatchedRequest())
+	})
+
+	t.Run("stops container on cancel", func(t *testing.T) {
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(Docker))
+		defer gock.Off()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images/docker.io/" + imageId + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ImageInspect{})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/create").
+			Reply(http.StatusOK).
+			JSON(container.ContainerCreateCreatedBody{ID: containerId})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/" + containerId + "/start").
+			Reply(http.StatusAccepted)
+		gock.New("http:///var/run/docker.sock").
+			Get("/v"+version+"/containers/"+containerId+"/logs").
+			Reply(http.StatusOK).
+			SetHeader("Content-Type", "application/vnd.docker.raw-stream").
+			Delay(1 * time.Second)
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/" + containerId + "/stop").
+			Reply(http.StatusServiceUnavailable)
+		// Run test
+		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(50*time.Millisecond))
+		_, err := DockerRunOnce(ctx, imageId, nil, nil)
+		cancel()
+		assert.Error(t, err)
+		assert.False(t, gock.HasUnmatchedRequest())
+	})
+
+	t.Run("throws error on failure to parse logs", func(t *testing.T) {
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(Docker))
+		defer gock.Off()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images/docker.io/" + imageId + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ImageInspect{})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/create").
+			Reply(http.StatusOK).
+			JSON(container.ContainerCreateCreatedBody{ID: containerId})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/" + containerId + "/start").
+			Reply(http.StatusAccepted)
+		gock.New("http:///var/run/docker.sock").
+			Get("/v"+version+"/containers/"+containerId+"/logs").
+			Reply(http.StatusOK).
+			SetHeader("Content-Type", "application/vnd.docker.raw-stream").
+			BodyString("hello world")
+		// Run test
+		_, err := DockerRunOnce(context.Background(), imageId, nil, nil)
+		assert.Error(t, err)
+		assert.False(t, gock.HasUnmatchedRequest())
+	})
+
+	t.Run("throws error on failure to inspect", func(t *testing.T) {
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(Docker))
+		defer gock.Off()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images/docker.io/" + imageId + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ImageInspect{})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/create").
+			Reply(http.StatusOK).
+			JSON(container.ContainerCreateCreatedBody{ID: containerId})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/" + containerId + "/start").
+			Reply(http.StatusAccepted)
+		// Setup docker style logs
+		var body bytes.Buffer
+		writer := stdcopy.NewStdWriter(&body, stdcopy.Stdout)
+		_, err := writer.Write([]byte("hello world"))
+		require.NoError(t, err)
+		gock.New("http:///var/run/docker.sock").
+			Get("/v"+version+"/containers/"+containerId+"/logs").
+			Reply(http.StatusOK).
+			SetHeader("Content-Type", "application/vnd.docker.raw-stream").
+			Body(&body)
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/containers/" + containerId + "/json").
+			Reply(http.StatusServiceUnavailable)
+		// Run test
+		_, err = DockerRunOnce(context.Background(), imageId, nil, nil)
+		assert.Error(t, err)
+		assert.False(t, gock.HasUnmatchedRequest())
+	})
+
+	t.Run("throws error on non-zero exit code", func(t *testing.T) {
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(Docker))
+		defer gock.Off()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images/docker.io/" + imageId + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ImageInspect{})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/create").
+			Reply(http.StatusOK).
+			JSON(container.ContainerCreateCreatedBody{ID: containerId})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/" + containerId + "/start").
+			Reply(http.StatusAccepted)
+		// Setup docker style logs
+		var body bytes.Buffer
+		writer := stdcopy.NewStdWriter(&body, stdcopy.Stdout)
+		_, err := writer.Write([]byte("hello world"))
+		require.NoError(t, err)
+		gock.New("http:///var/run/docker.sock").
+			Get("/v"+version+"/containers/"+containerId+"/logs").
+			Reply(http.StatusOK).
+			SetHeader("Content-Type", "application/vnd.docker.raw-stream").
+			Body(&body)
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/containers/" + containerId + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ContainerJSONBase{State: &types.ContainerState{ExitCode: 1}})
+		// Run test
+		_, err = DockerRunOnce(context.Background(), imageId, nil, nil)
+		assert.Error(t, err)
+		assert.False(t, gock.HasUnmatchedRequest())
+	})
+}
+
+func TestExecOnce(t *testing.T) {
+	t.Run("throws error on failure to exec", func(t *testing.T) {
+		// Setup mock server
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(Docker))
+		defer gock.Off()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/" + containerId + "/exec").
+			Reply(http.StatusServiceUnavailable)
+		// Run test
+		err := DockerExecOnce(context.Background(), containerId, nil, nil)
+		assert.Error(t, err)
+		assert.False(t, gock.HasUnmatchedRequest())
+	})
+
+	t.Run("throws error on failure to hijack", func(t *testing.T) {
+		// Setup mock server
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(Docker))
+		defer gock.Off()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/" + containerId + "/exec").
+			Reply(http.StatusAccepted).
+			JSON(types.IDResponse{ID: "test-command"})
+		// Run test
+		err := DockerExecOnce(context.Background(), containerId, nil, nil)
+		assert.Error(t, err)
+		assert.False(t, gock.HasUnmatchedRequest())
+	})
+
+	// TODO: mock tcp hijack
+}
