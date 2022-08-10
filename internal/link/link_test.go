9d342047575a514c935c6e2a684a3722f7db594e
Qiao Han <qiao@supabase.io>
2022-08-10 13:41:54 +0800

    chore: fix tests

--- internal/link/link_test.go
+++ internal/link/link_test.go
@@ -7,12 +7,11 @@ import (
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 	"github.com/supabase/cli/internal/testing/apitest"
-	"github.com/supabase/cli/internal/utils"
 	"gopkg.in/h2non/gock.v1"
 )
 
-func TestLinkCommand(t *testing.T) {
-	t.Run("link functions to project", func(t *testing.T) {
+func TestProjectValidation(t *testing.T) {
+	t.Run("no error if project ref is valid", func(t *testing.T) {
 		// Setup in-memory fs
 		project := apitest.RandomProjectRef()
 		fsys := afero.NewMemMapFs()
@@ -26,22 +25,22 @@ func TestLinkCommand(t *testing.T) {
 			Reply(200).
 			JSON([]string{})
 		// Run test
-		assert.NoError(t, Run(project, fsys))
+		assert.NoError(t, validateProjectRef(project, fsys))
 		// Validate file contents
-		content, err := afero.ReadFile(fsys, utils.ProjectRefPath)
-		assert.NoError(t, err)
-		assert.Equal(t, []byte(project), content)
+		// content, err := afero.ReadFile(fsys, utils.ProjectRefPath)
+		// assert.NoError(t, err)
+		// assert.Equal(t, []byte(project), content)
 	})
 
 	t.Run("throws error on invalid project ref", func(t *testing.T) {
-		assert.Error(t, Run("malformed", afero.NewMemMapFs()))
+		assert.Error(t, validateProjectRef("malformed", afero.NewMemMapFs()))
 	})
 
 	t.Run("throws error on failure to load token", func(t *testing.T) {
 		// Setup valid access token
 		project := apitest.RandomProjectRef()
 		fsys := afero.NewMemMapFs()
-		assert.Error(t, Run(project, fsys))
+		assert.Error(t, validateProjectRef(project, fsys))
 	})
 
 	t.Run("throws error on network error", func(t *testing.T) {
@@ -57,7 +56,7 @@ func TestLinkCommand(t *testing.T) {
 			Get("/v1/projects/" + project + "/functions").
 			ReplyError(errors.New("network error"))
 		// Run test
-		assert.Error(t, Run(project, fsys))
+		assert.Error(t, validateProjectRef(project, fsys))
 	})
 
 	t.Run("throws error on server unavailable", func(t *testing.T) {
@@ -74,23 +73,6 @@ func TestLinkCommand(t *testing.T) {
 			Reply(500).
 			JSON(map[string]string{"message": "unavailable"})
 		// Run test
-		assert.Error(t, Run(project, fsys))
-	})
-
-	t.Run("throws error on failure to create directory", func(t *testing.T) {
-		// Setup read-only fs
-		project := apitest.RandomProjectRef()
-		fsys := afero.NewReadOnlyFs(afero.NewMemMapFs())
-		// Setup valid access token
-		token := apitest.RandomAccessToken(t)
-		t.Setenv("SUPABASE_ACCESS_TOKEN", string(token))
-		// Flush pending mocks after test execution
-		defer gock.Off()
-		gock.New("https://api.supabase.io").
-			Get("/v1/projects/" + project + "/functions").
-			Reply(200).
-			JSON([]string{})
-		// Run test
-		assert.Error(t, Run(project, fsys))
+		assert.Error(t, validateProjectRef(project, fsys))
 	})
 }
