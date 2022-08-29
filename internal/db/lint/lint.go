19797b54a73d2b68ba8ec9baf2bd6712a2c7203c
Qiao Han <qiao@supabase.io>
2022-08-29 13:56:55 +0800

    feat: support multiple lint level and json output

--- internal/db/lint/lint.go
+++ internal/db/lint/lint.go
@@ -3,7 +3,9 @@ package lint
 import (
 	"context"
 	_ "embed"
+	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/jackc/pgx/v4"
@@ -14,11 +16,26 @@ import (
 )
 
 var (
+	AllowedLevels = []string{
+		"warning",
+		"error",
+	}
 	//go:embed templates/check.sql
 	checkSchemaScript string
 )
 
-func Run(ctx context.Context, schema []string, fsys afero.Fs) error {
+type LintLevel int
+
+func toEnum(level string) LintLevel {
+	for i, curr := range AllowedLevels {
+		if curr == level {
+			return LintLevel(i)
+		}
+	}
+	return -1
+}
+
+func Run(ctx context.Context, schema []string, level string, stdout io.Writer, fsys afero.Fs, opts ...func(*pgx.ConnConfig)) error {
 	// Sanity checks.
 	if err := utils.LoadConfigFS(fsys); err != nil {
 		return err
@@ -27,65 +44,131 @@ func Run(ctx context.Context, schema []string, fsys afero.Fs) error {
 		return err
 	}
 	// Run lint script
-	var opts []func(*pgx.ConnConfig)
-	if viper.GetBool("DEBUG") {
-		opts = append(opts, debug.SetupPGX)
+	conn, err := ConnectLocalPostgres(ctx, "localhost", utils.Config.Db.Port, "postgres", opts...)
+	if err != nil {
+		return err
 	}
-	url := fmt.Sprintf("postgresql://postgres:postgres@localhost:%d/postgres", utils.Config.Db.Port)
-	if err := LintDatabase(ctx, url, schema, opts...); err != nil {
+	defer conn.Close(ctx)
+	result, err := LintDatabase(ctx, conn, schema)
+	if err != nil {
+		return err
+	}
+	filtered := filterResult(result, toEnum(level))
+	// Print output
+	if len(filtered) == 0 {
+		return nil
+	}
+	enc := json.NewEncoder(stdout)
+	enc.SetIndent("", "  ")
+	if err := enc.Encode(filtered); err != nil {
 		return err
 	}
 	return nil
 }
 
-func LintDatabase(ctx context.Context, url string, schema []string, options ...func(*pgx.ConnConfig)) error {
+func filterResult(result []Result, minLevel LintLevel) (filtered []Result) {
+	for _, r := range result {
+		out := Result{Function: r.Function}
+		for _, issue := range r.Issues {
+			if toEnum(issue.Level) >= minLevel {
+				out.Issues = append(out.Issues, issue)
+			}
+		}
+		if len(out.Issues) > 0 {
+			filtered = append(filtered, out)
+		}
+	}
+	return filtered
+}
+
+// Connnect to local Postgres with optimised settings. The caller is responsible for closing the connection returned.
+func ConnectLocalPostgres(ctx context.Context, host string, port uint, database string, options ...func(*pgx.ConnConfig)) (*pgx.Conn, error) {
+	url := fmt.Sprintf("postgresql://postgres:postgres@%s:%d/%s", host, port, database)
 	// Parse connection url
 	config, err := pgx.ParseConfig(url)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	// Apply config overrides
 	for _, op := range options {
 		op(config)
 	}
+	if viper.GetBool("DEBUG") {
+		debug.SetupPGX(config)
+	}
 	// Connect to database
-	conn, err := pgx.ConnectConfig(ctx, config)
-	if err != nil {
-		return err
-	}
-	defer conn.Close(ctx)
+	return pgx.ConnectConfig(ctx, config)
+}
+
+func LintDatabase(ctx context.Context, conn *pgx.Conn, schema []string) ([]Result, error) {
 	// Enable plpgsql_check
-	enable := "CREATE EXTENSION IF NOT EXISTS plpgsql_check;"
+	enable := "CREATE EXTENSION IF NOT EXISTS plpgsql_check"
 	if _, err := conn.Exec(ctx, enable); err != nil {
-		return err
+		return nil, err
 	}
-	// Use prepared statements
-	if _, err := conn.Prepare(ctx, "ps1", checkSchemaScript); err != nil {
-		return err
-	}
+	// Batch prepares statements
 	batch := pgx.Batch{}
 	for _, s := range schema {
-		batch.Queue("ps1", s)
+		batch.Queue(checkSchemaScript, s)
 	}
 	br := conn.SendBatch(ctx, &batch)
 	defer br.Close()
+	var result []Result
 	for _, s := range schema {
 		fmt.Fprintln(os.Stderr, "Linting schema:", s)
 		rows, err := br.Query()
 		if err != nil {
-			return err
+			return nil, err
 		}
 		for rows.Next() {
-			v, err := rows.Values()
+			data := rows.RawValues()
+			r, err := toResult(data)
 			if err != nil {
-				return err
+				return nil, err
 			}
-			fmt.Println(v)
+			r.Function = s + "." + r.Function
+			result = append(result, r)
 		}
 		err = rows.Err()
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
-	return nil
+	return result, nil
+}
+
+func toResult(data [][]byte) (Result, error) {
+	var result Result
+	name := string(data[0])
+	if err := json.Unmarshal(data[1], &result); err != nil {
+		return result, err
+	}
+	result.Function = name
+	return result, nil
+}
+
+type Query struct {
+	Position string `json:"position"`
+	Text     string `json:"text"`
+}
+
+type Statement struct {
+	LineNumber string `json:"lineNumber"`
+	Text       string `json:"text"`
+}
+
+type Issue struct {
+	Level     string    `json:"level"`
+	Message   string    `json:"message"`
+	Statement Statement `json:"statement,omitempty"`
+	Query     Query     `json:"query,omitempty"`
+	Hint      string    `json:"hint,omitempty"`
+	Detail    string    `json:"detail,omitempty"`
+	Context   string    `json:"context,omitempty"`
+	SQLState  string    `json:"sqlState,omitempty"`
+}
+
+type Result struct {
+	Function string  `json:"function"`
+	Issues   []Issue `json:"issues"`
 }
