1761b921406f007916b4c02629e2e9df807f5687
Qiao Han <qiao@supabase.io>
2022-09-23 12:55:51 +0800

    chore: check services health in status command

--- internal/status/status.go
+++ internal/status/status.go
@@ -1,26 +1,50 @@
 package status
 
 import (
+	"context"
 	"fmt"
+	"os"
 
+	"github.com/spf13/afero"
 	"github.com/supabase/cli/internal/utils"
 )
 
-func Run() error {
+func Run(ctx context.Context, fsys afero.Fs) error {
 	// Sanity checks.
-	if err := utils.AssertDockerIsRunning(); err != nil {
-		return err
-	}
-	if err := utils.LoadConfig(); err != nil {
-		return err
-	}
-
-	if err := utils.AssertSupabaseStartIsRunning(); err == nil {
-		fmt.Println(utils.Aqua("supabase") + " local development setup is running.")
-		utils.ShowStatus()
-	} else {
-		fmt.Println(utils.Aqua("supabase") + " local development setup is not running.")
+	{
+		if err := utils.AssertSupabaseCliIsSetUpFS(fsys); err != nil {
+			return err
+		}
+		if err := utils.LoadConfigFS(fsys); err != nil {
+			return err
+		}
+		if err := utils.AssertDockerIsRunning(); err != nil {
+			return err
+		}
 	}
 
+	services := []string{
+		utils.DbId,
+		utils.KongId,
+		utils.GotrueId,
+		utils.InbucketId,
+		utils.RealtimeId,
+		utils.RestId,
+		utils.StorageId,
+		utils.PgmetaId,
+		utils.StudioId,
+	}
+	for _, name := range services {
+		resp, err := utils.Docker.ContainerInspect(ctx, name)
+		if err != nil {
+			return fmt.Errorf("container %s not found. Have your run %s?", name, utils.Aqua("supabase start"))
+		}
+		if !resp.State.Running {
+			fmt.Fprintln(os.Stderr, name, "container is not running:", resp.State.Status)
+		}
+	}
+
+	fmt.Fprintln(os.Stderr, utils.Aqua("supabase"), "local development setup is running.")
+	utils.ShowStatus()
 	return nil
 }
