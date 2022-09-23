ecb3c74eb9da40d3847a91267c904a7ac4289cbd
Qiao Han <qiao@supabase.io>
2022-09-23 13:34:08 +0800

    chore: add status tests

---
+++ internal/status/status_test.go
@@ -0,0 +1,123 @@
+package status
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/supabase/cli/internal/testing/apitest"
+	"github.com/supabase/cli/internal/utils"
+	"gopkg.in/h2non/gock.v1"
+)
+
+func TestStatusCommand(t *testing.T) {
+	const version = "1.41"
+
+	t.Run("throws error on missing config", func(t *testing.T) {
+		err := Run(context.Background(), afero.NewMemMapFs())
+		assert.ErrorContains(t, err, "Have you set up the project with supabase init?")
+	})
+
+	t.Run("throws error on invalid config", func(t *testing.T) {
+		// Setup in-memory fs
+		fsys := afero.NewMemMapFs()
+		require.NoError(t, afero.WriteFile(fsys, utils.ConfigPath, []byte("malformed"), 0644))
+		// Run test
+		err := Run(context.Background(), fsys)
+		// Check error
+		assert.ErrorContains(t, err, "Failed to read config: toml")
+	})
+
+	t.Run("throws error on missing docker", func(t *testing.T) {
+		// Setup in-memory fs
+		fsys := afero.NewMemMapFs()
+		require.NoError(t, utils.WriteConfig(fsys, false))
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(utils.Docker))
+		defer gock.OffAll()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			ReplyError(errors.New("network error"))
+		gock.New("http:///var/run/docker.sock").
+			Get("/_ping").
+			ReplyError(errors.New("network error"))
+		// Run test
+		err := Run(context.Background(), fsys)
+		// Check error
+		assert.ErrorContains(t, err, "network error")
+		assert.Empty(t, apitest.ListUnmatchedRequests())
+	})
+
+	t.Run("throws error on missing container", func(t *testing.T) {
+		// Setup in-memory fs
+		fsys := afero.NewMemMapFs()
+		require.NoError(t, utils.WriteConfig(fsys, false))
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(utils.Docker))
+		defer gock.OffAll()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/containers").
+			Reply(http.StatusNotFound)
+		// Run test
+		Run(context.Background(), fsys)
+		// Check error
+		assert.Empty(t, apitest.ListUnmatchedRequests())
+	})
+}
+
+func TestServiceHealth(t *testing.T) {
+	const version = "1.41"
+
+	t.Run("checks all services", func(t *testing.T) {
+		services := []string{"supabase_db", "supabase_auth"}
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(utils.Docker))
+		defer gock.OffAll()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/containers/" + services[0] + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ContainerJSON{ContainerJSONBase: &types.ContainerJSONBase{
+				State: &types.ContainerState{Running: true},
+			}})
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/containers/" + services[1] + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ContainerJSON{ContainerJSONBase: &types.ContainerJSONBase{
+				State: &types.ContainerState{Status: "exited"},
+			}})
+		// Run test
+		var stderr bytes.Buffer
+		assert.NoError(t, showServiceHealth(context.Background(), services, &stderr))
+		// Check error
+		assert.Contains(t, stderr.String(), "supabase_auth container is not running: exited")
+		assert.Contains(t, stderr.String(), "supabase local development setup is running.")
+		assert.Empty(t, apitest.ListUnmatchedRequests())
+	})
+}
