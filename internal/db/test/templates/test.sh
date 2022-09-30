40330cf14a20b7174eaf58088ae2420d5c89effd
Qiao Han <qiao@supabase.io>
2022-10-01 00:46:17 +0800

    chore: enable pgtap on demand

--- internal/db/test/templates/test.sh
+++ internal/db/test/templates/test.sh
@@ -1,14 +1,25 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
 
-# move this dependency to base image
-apt-get -qq update && apt-get -qq install postgresql-14-pgtap
+# TODO: move this dependency to base image
+if ! command -v pg_prove &> /dev/null; then
+    apt-get -qq update && apt-get -qq install postgresql-14-pgtap
+fi
+
+# temporarily enable pgtap
+enable="create extension if not exists pgtap with schema extensions"
+notice=$(psql -h localhost -U postgres -p 5432 -d postgres -c "$enable" 2>&1 >/dev/null)
 
 # run postgres unit tests
-pg_prove -h localhost -U postgres "$@"
+pg_prove -h localhost -U postgres -r "$@"
 
 cleanup() {
     # save the return code of the script
     status=$?
+    # disable pgtap
+    if [ -z "$notice" ]; then
+        psql -h localhost -U postgres -p 5432 -d postgres -c "drop extension if exists pgtap"
+    fi
     # clean up test files
     rm -rf "$@"
     # actually quit
