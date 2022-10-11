24a0527e8f56f2f5db4a2238a5376bb67d39147d
Qiao Han <qiao@supabase.io>
2022-10-11 13:54:47 +0800

    chore: add image pull and container tests

--- internal/start/start_test.go
+++ internal/start/start_test.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"errors"
 	"net/http"
+	"path/filepath"
 	"testing"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
@@ -123,6 +125,107 @@ func TestStartCommand(t *testing.T) {
 		err := Run(context.Background(), fsys)
 		// Check error
 		assert.ErrorContains(t, err, "network error")
+		assert.Empty(t, apitest.ListUnmatchedRequests())
+	})
+}
+
+func TestPullImage(t *testing.T) {
+	const version = "1.41"
+	const image = "postgres"
+	p := utils.NewProgram(model{})
+
+	t.Run("inspects image before pull", func(t *testing.T) {
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(utils.Docker))
+		defer gock.OffAll()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images/public.ecr.aws/t3w2s2c9/" + image + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ImageInspect{})
+		// Run test
+		err := pullImage(p, context.Background(), image)
+		// Check error
+		assert.NoError(t, err)
+		assert.Empty(t, apitest.ListUnmatchedRequests())
+	})
+
+	t.Run("pulls missing image", func(t *testing.T) {
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(utils.Docker))
+		defer gock.OffAll()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images/public.ecr.aws/t3w2s2c9/" + image + "/json").
+			Reply(http.StatusNotFound)
+		gock.New("http:///var/run/docker.sock").
+			Post("/v"+version+"/images/create").
+			MatchParam("fromImage", image).
+			MatchParam("tag", "latest").
+			Reply(http.StatusAccepted).
+			BodyString("progress")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images/public.ecr.aws/t3w2s2c9/" + image + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ImageInspect{})
+		// Run test
+		err := pullImage(p, context.Background(), image)
+		// Check error
+		assert.NoError(t, err)
+		assert.Empty(t, apitest.ListUnmatchedRequests())
+	})
+
+	// TODO: test for transient error
+}
+
+func TestDatabaseStart(t *testing.T) {
+	const version = "1.41"
+	p := utils.NewProgram(model{})
+
+	t.Run("starts database locally", func(t *testing.T) {
+		// Setup in-memory fs
+		fsys := afero.NewMemMapFs()
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(utils.Docker))
+		defer gock.OffAll()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/networks/create").
+			Reply(http.StatusCreated).
+			JSON(types.NetworkCreateResponse{})
+		// Caches all dependencies
+		utils.DbImage = "postgres"
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images/public.ecr.aws/t3w2s2c9/" + utils.DbImage + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ImageInspect{})
+		for _, image := range utils.ServiceImages {
+			service := filepath.Base(image)
+			gock.New("http:///var/run/docker.sock").
+				Get("/v" + version + "/images/public.ecr.aws/t3w2s2c9/" + service + "/json").
+				Reply(http.StatusOK).
+				JSON(types.ImageInspect{})
+		}
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/create").
+			Reply(http.StatusCreated).
+			JSON(container.ContainerCreateCreatedBody{})
+		// Run test
+		err := run(p, context.Background(), fsys)
+		// Check error
+		assert.ErrorContains(t, err, "unable to upgrade to tcp, received 404")
 		assert.Empty(t, apitest.ListUnmatchedRequests())
 	})
 }
