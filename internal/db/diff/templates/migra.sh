0fa43a51dbba2ab86a985fbed2afd94d70f40732
Qiao Han <qiao@supabase.io>
2022-10-12 11:14:51 +0800

    chore: update migra to latest version

--- internal/db/diff/templates/migra.sh
+++ internal/db/diff/templates/migra.sh
@@ -2,11 +2,7 @@
 set -eu
 
 # pin to latest version: https://pypi.org/project/migra/
-pip install -qU migra==3.0.1658662267
-
-# fix enum: https://github.com/djrobstep/schemainspect/pull/63
-sed -i 's/  and e.objid is null/  -- and e.objid is null/g' \
-/usr/local/lib/python3.9/site-packages/schemainspect/pg/sql/enums.sql
+pip install -qU migra==3.0.1663481299
 
 # migra doesn't shutdown gracefully, so kill it ourselves
 trap 'kill -9 %1' TERM
