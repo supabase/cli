06b1ab0e2b10a994a512bd9afd12bed675dbe9e1
Qiao Han <qiao@supabase.io>
2022-10-13 02:43:02 +0800

    chore: remove port arg

--- internal/db/remote/commit/commit.go
+++ internal/db/remote/commit/commit.go
@@ -274,8 +274,8 @@ EOSQL
 	{
 		p.Send(utils.StatusMsg("Committing changes on remote database as a new migration..."))
 
-		src := fmt.Sprintf(`"dbname='%s' user='%s' host='%s' port=5432 password='%s'"`, database, username, host, password)
-		dst := fmt.Sprintf(`"dbname='%s' user=postgres host='%s' port=5432 password=postgres"`, utils.ShadowDbName, dbId)
+		src := fmt.Sprintf(`"dbname='%s' user='%s' host='%s' password='%s'"`, database, username, host, password)
+		dst := fmt.Sprintf(`"dbname='%s' user=postgres host='%s' password=postgres"`, utils.ShadowDbName, dbId)
 		out, err := utils.DockerRun(
 			ctx,
 			differId,
