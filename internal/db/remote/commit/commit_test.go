41851d29d169557b9216a3edd1c2983f21352c36
Bobbie Soedirgo <bobbie@soedirgo.dev>
2022-10-12 19:32:42 +0800

    fix: improper url-escaping

---
+++ internal/db/remote/commit/commit_test.go
@@ -0,0 +1,36 @@
+package commit
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/supabase/cli/internal/testing/pgtest"
+)
+
+func TestConnectRemotePostgres(t *testing.T) {
+	t.Run("connects to remote postgres successfully", func(t *testing.T) {
+		// Setup mock postgres
+		conn := pgtest.NewConn()
+		defer conn.Close(t)
+
+		// Run test
+		c, err := ConnectRemotePostgres(context.Background(), "username", "password", "database", "localhost", conn.Intercept)
+		defer c.Close(context.Background())
+		assert.NoError(t, err)
+	})
+
+	t.Run("preserves db password", func(t *testing.T) {
+		// Setup mock postgres
+		conn := pgtest.NewConn()
+		defer conn.Close(t)
+
+		// Run test
+		password := "pass word"
+		c, err := ConnectRemotePostgres(context.Background(), "username", password, "database", "localhost", conn.Intercept)
+		defer c.Close(context.Background())
+		require.NoError(t, err)
+		assert.Equal(t, password, c.Config().Password)
+	})
+}
