5e5d803864a018384d8ffd10fb0d06950fe39617
Han Qiao <sweatybridge@gmail.com>
2022-10-14 11:04:48 +0800

    fix: add backup flag to stop command (#521)

--- internal/stop/stop_test.go
+++ internal/stop/stop_test.go
@@ -1,12 +1,17 @@
 package stop
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"net/http"
+	"path/filepath"
 	"testing"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -44,7 +49,7 @@ func TestStopCommand(t *testing.T) {
 			Reply(http.StatusOK).
 			JSON(types.NetworksPruneReport{})
 		// Run test
-		err := Run(context.Background(), fsys)
+		err := Run(context.Background(), false, fsys)
 		// Check error
 		assert.NoError(t, err)
 		assert.Empty(t, apitest.ListUnmatchedRequests())
@@ -58,7 +63,7 @@ func TestStopCommand(t *testing.T) {
 		// Setup in-memory fs
 		fsys := afero.NewMemMapFs()
 		// Run test
-		err := Run(context.Background(), fsys)
+		err := Run(context.Background(), false, fsys)
 		// Check error
 		assert.ErrorContains(t, err, "Missing config: open supabase/config.toml: file does not exist")
 	})
@@ -79,9 +84,35 @@ func TestStopCommand(t *testing.T) {
 			Get("/v" + version + "/containers").
 			Reply(http.StatusServiceUnavailable)
 		// Run test
-		err := Run(context.Background(), fsys)
+		err := Run(context.Background(), false, fsys)
 		// Check error
 		assert.NoError(t, err)
+		assert.Empty(t, apitest.ListUnmatchedRequests())
+	})
+
+	t.Run("throws error on backup failure", func(t *testing.T) {
+		// Setup in-memory fs
+		fsys := afero.NewMemMapFs()
+		require.NoError(t, utils.WriteConfig(fsys, false))
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(utils.Docker))
+		defer gock.OffAll()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/containers").
+			Reply(http.StatusOK).
+			JSON(types.ContainerJSON{})
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images").
+			ReplyError(errors.New("network error"))
+		// Run test
+		err := Run(context.Background(), true, afero.NewReadOnlyFs(fsys))
+		// Check error
+		assert.ErrorContains(t, err, "network error")
 		assert.Empty(t, apitest.ListUnmatchedRequests())
 	})
 
@@ -105,13 +136,13 @@ func TestStopCommand(t *testing.T) {
 			Get("/v" + version + "/containers/json").
 			Reply(http.StatusServiceUnavailable)
 		// Run test
-		err := Run(context.Background(), afero.NewReadOnlyFs(fsys))
+		err := Run(context.Background(), false, afero.NewReadOnlyFs(fsys))
 		// Check error
 		assert.ErrorContains(t, err, "request returned Service Unavailable for API route and version")
 		assert.Empty(t, apitest.ListUnmatchedRequests())
 	})
 
-	t.Run("throws error on permission denied", func(t *testing.T) {
+	t.Run("ignores permission error", func(t *testing.T) {
 		// Setup in-memory fs
 		fsys := afero.NewMemMapFs()
 		require.NoError(t, utils.WriteConfig(fsys, false))
@@ -136,9 +167,9 @@ func TestStopCommand(t *testing.T) {
 			Reply(http.StatusOK).
 			JSON(types.NetworksPruneReport{})
 		// Run test
-		err := Run(context.Background(), afero.NewReadOnlyFs(fsys))
+		err := Run(context.Background(), false, afero.NewReadOnlyFs(fsys))
 		// Check error
-		assert.ErrorContains(t, err, "operation not permitted")
+		assert.NoError(t, err)
 		assert.Empty(t, apitest.ListUnmatchedRequests())
 	})
 }
@@ -192,6 +223,114 @@ func TestStopServices(t *testing.T) {
 		err := stop(context.Background())
 		// Check error
 		assert.ErrorContains(t, err, "request returned Service Unavailable for API route and version")
+		assert.Empty(t, apitest.ListUnmatchedRequests())
+	})
+}
+
+func TestBackupDatabase(t *testing.T) {
+	const version = "1.41"
+	const containerId = "test-db"
+
+	t.Run("backup main branch", func(t *testing.T) {
+		image := utils.GetRegistryImageUrl(utils.Pg14Image)
+		// Setup in-memory fs
+		fsys := afero.NewMemMapFs()
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(utils.Docker))
+		defer gock.OffAll()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images/" + image + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ImageInspect{})
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/networks").
+			Reply(http.StatusOK).
+			JSON(types.NetworkResource{})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/create").
+			Reply(http.StatusOK).
+			JSON(container.ContainerCreateCreatedBody{ID: containerId})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/" + containerId + "/start").
+			Reply(http.StatusAccepted)
+		// Setup docker style logs
+		var body bytes.Buffer
+		writer := stdcopy.NewStdWriter(&body, stdcopy.Stdout)
+		dumped := []byte("create schema public")
+		_, err := writer.Write(dumped)
+		require.NoError(t, err)
+		gock.New("http:///var/run/docker.sock").
+			Get("/v"+version+"/containers/"+containerId+"/logs").
+			Reply(http.StatusOK).
+			SetHeader("Content-Type", "application/vnd.docker.raw-stream").
+			Body(&body)
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/containers/" + containerId + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ContainerJSONBase{State: &types.ContainerState{ExitCode: 0}})
+		// Run test
+		err = backupDatabase(context.Background(), fsys)
+		// Check error
+		assert.NoError(t, err)
+		assert.Empty(t, apitest.ListUnmatchedRequests())
+		path := filepath.Join(filepath.Dir(utils.CurrBranchPath), "main", "dump.sql")
+		// Check branch dumped
+		contents, err := afero.ReadFile(fsys, path)
+		assert.NoError(t, err)
+		assert.Equal(t, dumped, contents)
+	})
+
+	t.Run("throws error on permission denied", func(t *testing.T) {
+		image := utils.GetRegistryImageUrl(utils.Pg14Image)
+		// Setup in-memory fs
+		fsys := afero.NewReadOnlyFs(afero.NewMemMapFs())
+		// Setup mock docker
+		require.NoError(t, client.WithHTTPClient(http.DefaultClient)(utils.Docker))
+		defer gock.OffAll()
+		gock.New("http:///var/run/docker.sock").
+			Head("/_ping").
+			Reply(http.StatusOK).
+			SetHeader("API-Version", version).
+			SetHeader("OSType", "linux")
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/images/" + image + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ImageInspect{})
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/networks").
+			Reply(http.StatusOK).
+			JSON(types.NetworkResource{})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/create").
+			Reply(http.StatusOK).
+			JSON(container.ContainerCreateCreatedBody{ID: containerId})
+		gock.New("http:///var/run/docker.sock").
+			Post("/v" + version + "/containers/" + containerId + "/start").
+			Reply(http.StatusAccepted)
+		// Setup docker style logs
+		var body bytes.Buffer
+		writer := stdcopy.NewStdWriter(&body, stdcopy.Stdout)
+		dumped := []byte("create schema public")
+		_, err := writer.Write(dumped)
+		require.NoError(t, err)
+		gock.New("http:///var/run/docker.sock").
+			Get("/v"+version+"/containers/"+containerId+"/logs").
+			Reply(http.StatusOK).
+			SetHeader("Content-Type", "application/vnd.docker.raw-stream").
+			Body(&body)
+		gock.New("http:///var/run/docker.sock").
+			Get("/v" + version + "/containers/" + containerId + "/json").
+			Reply(http.StatusOK).
+			JSON(types.ContainerJSONBase{State: &types.ContainerState{ExitCode: 0}})
+		// Run test
+		err = backupDatabase(context.Background(), fsys)
+		// Check error
+		assert.ErrorContains(t, err, "operation not permitted")
 		assert.Empty(t, apitest.ListUnmatchedRequests())
 	})
 }
