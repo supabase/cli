abbe9a73f43e9132e11c51e9f5d4fd4f01aa8dec
Qiao Han <qiao@supabase.io>
2022-10-24 12:41:55 +0800

    fix: error in getting project root

--- internal/utils/misc_test.go
+++ internal/utils/misc_test.go
@@ -1,14 +1,28 @@
 package utils
 
 import (
+	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+type MockFs struct {
+	afero.MemMapFs
+	DenyPath string
+}
+
+func (m *MockFs) Stat(name string) (fs.FileInfo, error) {
+	if strings.HasPrefix(name, m.DenyPath) {
+		return nil, fs.ErrPermission
+	}
+	return m.MemMapFs.Stat(name)
+}
 
 func TestProjectRoot(t *testing.T) {
 	t.Run("searches project root recursively", func(t *testing.T) {
@@ -28,6 +42,18 @@ func TestProjectRoot(t *testing.T) {
 		require.NoError(t, err)
 		// Setup in-memory fs
 		fsys := afero.NewMemMapFs()
+		// Run test
+		path, err := GetProjectRoot(fsys)
+		// Check error
+		assert.NoError(t, err)
+		assert.Equal(t, cwd, path)
+	})
+
+	t.Run("ignores error if path is not directory", func(t *testing.T) {
+		cwd, err := os.Getwd()
+		require.NoError(t, err)
+		// Setup in-memory fs
+		fsys := &MockFs{DenyPath: filepath.Join(cwd, "supabase")}
 		// Run test
 		path, err := GetProjectRoot(fsys)
 		// Check error
