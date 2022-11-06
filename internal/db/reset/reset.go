387bedd20c04ab81d7171875bb7cded108b3e3a2
Qiao Han <qiao@supabase.io>
2022-11-06 22:13:39 +0800

    fix: null pointer dereference on checking container health

--- internal/db/reset/reset.go
+++ internal/db/reset/reset.go
@@ -162,7 +162,10 @@ func WaitForHealthyDatabase(ctx context.Context, timeout time.Duration) bool {
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 	for t := now; t.Before(expiry); t = <-ticker.C {
-		if resp, err := utils.Docker.ContainerInspect(ctx, utils.DbId); err == nil && resp.State.Health.Status == "healthy" {
+		if resp, err := utils.Docker.ContainerInspect(ctx, utils.DbId); err == nil &&
+			resp.State != nil &&
+			resp.State.Health != nil &&
+			resp.State.Health.Status == "healthy" {
 			return true
 		}
 	}
