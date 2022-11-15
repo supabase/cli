6ccc1e998bd32a10c5a3e15eb83ae75179d7c677
Nathan Tamez <nathan@tamez.email>
2022-11-15 07:43:28 +0000

    feat: support output variables with status command (#452)
    * Add ability to export values on `start`
    
    * feat(start): Add ability to export values on `start`
    
    this commit added a new flag on `supabase start`, `--value-flag`
    
    * updated utils.ShowStatus to print to stdout
    
    * fix logs to stdout bug
    
    logs now print to stderr, this allows for stdout to be redirected
    
    * chore: undo config changes
    
    * feat: support status variable output
    
    * chore: remove env var from test config
    
    * chore: add status example
    
    * chore: revert start command changes
    
    Co-authored-by: Qiao Han <qiao@supabase.io>
    Co-authored-by: Han Qiao <sweatybridge@gmail.com>

--- cmd/status.go
+++ cmd/status.go
@@ -4,21 +4,50 @@ import (
 	"os"
 	"os/signal"
 
+	env "github.com/Netflix/go-env"
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 	"github.com/supabase/cli/internal/status"
+	"github.com/supabase/cli/internal/utils"
 )
 
-var statusCmd = &cobra.Command{
-	GroupID: groupLocalDev,
-	Use:     "status",
-	Short:   "Show status of local Supabase containers",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		ctx, _ := signal.NotifyContext(cmd.Context(), os.Interrupt)
-		return status.Run(ctx, afero.NewOsFs())
-	},
-}
+var (
+	override []string
+	names    status.CustomName
+	output   = utils.EnumFlag{
+		Allowed: []string{
+			status.OutputEnv,
+			status.OutputJson,
+			status.OutputPretty,
+			status.OutputToml,
+			status.OutputYaml,
+		},
+		Value: status.OutputPretty,
+	}
+
+	statusCmd = &cobra.Command{
+		GroupID: groupLocalDev,
+		Use:     "status",
+		Short:   "Show status of local Supabase containers",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			es, err := env.EnvironToEnvSet(override)
+			if err != nil {
+				return err
+			}
+			return env.Unmarshal(es, &names)
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx, _ := signal.NotifyContext(cmd.Context(), os.Interrupt)
+			return status.Run(ctx, names, output.Value, afero.NewOsFs())
+		},
+		Example: `  supabase status -o env --override-name api.url=NEXT_PUBLIC_SUPABASE_URL
+  supabase status -o json`,
+	}
+)
 
 func init() {
+	flags := statusCmd.Flags()
+	flags.VarP(&output, "output", "o", "Output format of status variables.")
+	flags.StringSliceVar(&override, "override-name", []string{}, "Override specific variable names.")
 	rootCmd.AddCommand(statusCmd)
 }
