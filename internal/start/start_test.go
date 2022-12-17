b9ae36f47530208d637ad963db1c3baf033c69e9
Han Qiao <sweatybridge@gmail.com>
2022-12-17 21:44:12 +0800

    fix: unify image pull with retry implementation (#713)
    * chore: log image url on pull retry
    
    * chore: unify image pull with retry
    
    * chore: update image pull tests

--- internal/start/start_test.go
+++ internal/start/start_test.go
@@ -7,7 +7,6 @@ import (
 	"testing"
 
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/pkg/jsonmessage"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -112,95 +111,6 @@ func TestStartCommand(t *testing.T) {
 		err := Run(context.Background(), fsys, []string{})
 		// Check error
 		assert.ErrorContains(t, err, "network error")
-		assert.Empty(t, apitest.ListUnmatchedRequests())
-	})
-}
-
-func TestPullImage(t *testing.T) {
-	const image = "postgres"
-	imageUrl := utils.GetRegistryImageUrl(image)
-	p := utils.NewProgram(model{})
-
-	t.Run("inspects image before pull", func(t *testing.T) {
-		// Setup mock docker
-		require.NoError(t, apitest.MockDocker(utils.Docker))
-		defer gock.OffAll()
-		gock.New(utils.Docker.DaemonHost()).
-			Get("/v" + utils.Docker.ClientVersion() + "/images/" + imageUrl + "/json").
-			Reply(http.StatusOK).
-			JSON(types.ImageInspect{})
-		// Run test
-		err := pullImage(p, context.Background(), image)
-		// Check error
-		assert.NoError(t, err)
-		assert.Empty(t, apitest.ListUnmatchedRequests())
-	})
-
-	t.Run("pulls missing image", func(t *testing.T) {
-		// Setup mock docker
-		require.NoError(t, apitest.MockDocker(utils.Docker))
-		defer gock.OffAll()
-		gock.New(utils.Docker.DaemonHost()).
-			Get("/v" + utils.Docker.ClientVersion() + "/images/" + imageUrl + "/json").
-			Reply(http.StatusNotFound)
-		gock.New(utils.Docker.DaemonHost()).
-			Post("/v"+utils.Docker.ClientVersion()+"/images/create").
-			MatchParam("fromImage", image).
-			MatchParam("tag", "latest").
-			Reply(http.StatusAccepted).
-			BodyString("")
-		gock.New(utils.Docker.DaemonHost()).
-			Get("/v" + utils.Docker.ClientVersion() + "/images/" + imageUrl + "/json").
-			Reply(http.StatusOK).
-			JSON(types.ImageInspect{})
-		// Run test
-		err := pullImage(p, context.Background(), image)
-		// Check error
-		assert.NoError(t, err)
-		assert.Empty(t, apitest.ListUnmatchedRequests())
-	})
-
-	t.Run("throws error on pull failure", func(t *testing.T) {
-		pullRetry = 0
-		// Setup mock docker
-		require.NoError(t, apitest.MockDocker(utils.Docker))
-		defer gock.OffAll()
-		gock.New(utils.Docker.DaemonHost()).
-			Get("/v" + utils.Docker.ClientVersion() + "/images/" + imageUrl + "/json").
-			Reply(http.StatusNotFound)
-		gock.New(utils.Docker.DaemonHost()).
-			Post("/v"+utils.Docker.ClientVersion()+"/images/create").
-			MatchParam("fromImage", image).
-			MatchParam("tag", "latest").
-			Reply(http.StatusServiceUnavailable)
-		// Run test
-		err := pullImage(p, context.Background(), image)
-		// Check error
-		assert.ErrorContains(t, err, "request returned Service Unavailable")
-		assert.Empty(t, apitest.ListUnmatchedRequests())
-	})
-
-	t.Run("throws error on no space left", func(t *testing.T) {
-		// Setup mock docker
-		require.NoError(t, apitest.MockDocker(utils.Docker))
-		defer gock.OffAll()
-		gock.New(utils.Docker.DaemonHost()).
-			Get("/v" + utils.Docker.ClientVersion() + "/images/" + imageUrl + "/json").
-			Reply(http.StatusNotFound)
-		gock.New(utils.Docker.DaemonHost()).
-			Post("/v"+utils.Docker.ClientVersion()+"/images/create").
-			MatchParam("fromImage", image).
-			MatchParam("tag", "latest").
-			Reply(http.StatusAccepted).
-			JSON(jsonmessage.JSONMessage{Error: &jsonmessage.JSONError{Message: "no space left on device"}})
-		gock.New(utils.Docker.DaemonHost()).
-			Get("/v" + utils.Docker.ClientVersion() + "/images/" + imageUrl + "/json").
-			Reply(http.StatusOK).
-			JSON(types.ImageInspect{})
-		// Run test
-		err := pullImage(p, context.Background(), image)
-		// Check error
-		assert.ErrorContains(t, err, "no space left on device")
 		assert.Empty(t, apitest.ListUnmatchedRequests())
 	})
 }
--- internal/utils/docker_test.go
+++ internal/utils/docker_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/pkg/jsonmessage"
 	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
@@ -72,6 +73,7 @@ func TestPullImage(t *testing.T) {
 	})
 
 	t.Run("throws error on failure to pull image", func(t *testing.T) {
+		timeUnit = time.Duration(0)
 		// Setup mock docker
 		require.NoError(t, apitest.MockDocker(Docker))
 		defer gock.OffAll()
@@ -88,15 +90,18 @@ func TestPullImage(t *testing.T) {
 			Post("/v"+Docker.ClientVersion()+"/images/create").
 			MatchParam("fromImage", imageId).
 			MatchParam("tag", "latest").
-			Reply(http.StatusServiceUnavailable)
+			Reply(http.StatusAccepted).
+			JSON(jsonmessage.JSONMessage{Error: &jsonmessage.JSONError{Message: "toomanyrequests"}})
 		gock.New(Docker.DaemonHost()).
 			Post("/v"+Docker.ClientVersion()+"/images/create").
 			MatchParam("fromImage", imageId).
 			MatchParam("tag", "latest").
-			Reply(http.StatusServiceUnavailable)
+			Reply(http.StatusAccepted).
+			JSON(jsonmessage.JSONMessage{Error: &jsonmessage.JSONError{Message: "no space left on device"}})
 		// Run test
-		assert.Error(t, DockerPullImageIfNotCached(context.Background(), imageId))
+		err := DockerPullImageIfNotCached(context.Background(), imageId)
 		// Validate api
+		assert.ErrorContains(t, err, "no space left on device")
 		assert.Empty(t, apitest.ListUnmatchedRequests())
 	})
 }
