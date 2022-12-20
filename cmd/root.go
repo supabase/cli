6c4c6ec2a4766dfd18a59561136e0a590cf72b43
Qiao Han <qiao@supabase.io>
2022-12-20 11:05:41 +0800

    chore: declare experimental commands in one place

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,13 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strings"
 
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"github.com/supabase/cli/internal/utils"
 )
@@ -17,34 +17,51 @@ const (
 	groupManagementAPI = "management-api"
 )
 
+var experimental = []*cobra.Command{
+	bansCmd,
+	restrictionsCmd,
+	vanityCmd,
+}
+
+func IsExperimental(cmd *cobra.Command) bool {
+	for _, exp := range experimental {
+		if cmd == exp || cmd.Parent() == exp {
+			return true
+		}
+	}
+	return false
+}
+
 var (
 	// Passed from `-ldflags`: https://stackoverflow.com/q/11354518.
-	version      string
-	experimental bool
-	suggestion   string
-)
+	version    string
+	suggestion string
 
-var rootCmd = &cobra.Command{
-	Use:     "supabase",
-	Short:   "Supabase CLI " + version,
-	Version: version,
-	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		cmd.SilenceUsage = true
-		if viper.GetBool("DEBUG") {
-			cmd.SetContext(utils.WithTraceContext(cmd.Context()))
-		} else {
-			suggestion = "Try rerunning the command with --debug to troubleshoot the error."
-		}
-		workdir := viper.GetString("WORKDIR")
-		if workdir == "" {
-			var err error
-			if workdir, err = utils.GetProjectRoot(afero.NewOsFs()); err != nil {
-				return err
+	rootCmd = &cobra.Command{
+		Use:     "supabase",
+		Short:   "Supabase CLI " + version,
+		Version: version,
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			if IsExperimental(cmd) && !viper.GetBool("experimental") {
+				return errors.New("must set the --experimental flag to run this command")
 			}
-		}
-		return os.Chdir(workdir)
-	},
-}
+			cmd.SilenceUsage = true
+			if viper.GetBool("DEBUG") {
+				cmd.SetContext(utils.WithTraceContext(cmd.Context()))
+			} else {
+				suggestion = "Try rerunning the command with --debug to troubleshoot the error."
+			}
+			workdir := viper.GetString("WORKDIR")
+			if workdir == "" {
+				var err error
+				if workdir, err = utils.GetProjectRoot(afero.NewOsFs()); err != nil {
+					return err
+				}
+			}
+			return os.Chdir(workdir)
+		},
+	}
+)
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
@@ -58,19 +75,15 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(func() {
 		viper.SetEnvPrefix("SUPABASE")
+		viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 		viper.AutomaticEnv()
 	})
-	flags := rootCmd.PersistentFlags()
 
+	flags := rootCmd.PersistentFlags()
 	flags.Bool("debug", false, "output debug logs to stderr")
 	flags.String("workdir", "", "path to a Supabase project directory")
-
-	flags.VisitAll(func(f *pflag.Flag) {
-		key := strings.ReplaceAll(f.Name, "-", "_")
-		cobra.CheckErr(viper.BindPFlag(key, flags.Lookup(f.Name)))
-	})
-
-	flags.BoolVar(&experimental, "experimental", false, "enable experimental features")
+	flags.Bool("experimental", false, "enable experimental features")
+	cobra.CheckErr(viper.BindPFlags(flags))
 
 	rootCmd.SetVersionTemplate("{{.Version}}\n")
 	rootCmd.AddGroup(&cobra.Group{ID: groupLocalDev, Title: "Local Development:"})
