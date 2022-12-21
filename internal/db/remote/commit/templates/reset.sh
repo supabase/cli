8262d612bc2bfb2f1cfd2846401cf7d7d5f1bc00
Qiao Han <qiao@supabase.io>
2022-12-21 17:14:32 +0800

    chore: remove unused scripts

--- internal/db/remote/commit/templates/reset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# recreate shadow database from scratch
-dropdb --username postgres --host 127.0.0.1 --if-exists "$DB_NAME"
-createdb --username postgres --host 127.0.0.1 "$DB_NAME"
-
-# initialise large schema here to avoid lockup
-psql --username postgres --host 127.0.0.1 -d "$DB_NAME" -c "$SCHEMA"
