9591316f83da6ee82deb9d86fdbe2dc1041b75d5
Qiao Han <qiao@supabase.io>
2022-12-21 16:14:58 +0800

    fix: unify shadow creation for remote commands

--- internal/db/diff/diff.go
+++ internal/db/diff/diff.go
@@ -1,43 +1,35 @@
 package diff
 
 import (
-	"bytes"
 	"context"
 	_ "embed"
-	"errors"
 	"fmt"
-	"io"
 	"os"
-	"regexp"
-	"strconv"
-	"strings"
 
-	"github.com/charmbracelet/bubbles/progress"
-	"github.com/charmbracelet/bubbles/spinner"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
-	"github.com/docker/docker/pkg/stdcopy"
-	"github.com/muesli/reflow/wrap"
 	"github.com/spf13/afero"
-	"github.com/supabase/cli/internal/db/remote/commit"
 	"github.com/supabase/cli/internal/migration/new"
 	"github.com/supabase/cli/internal/utils"
 )
+
+var warnDiff = `WARNING: The diff tool is not foolproof, so you may need to manually rearrange and modify the generated migration.
+Run ` + utils.Aqua("supabase db reset") + ` to verify that the new migration does not generate errors.`
 
 func SaveDiff(out, file string, fsys afero.Fs) error {
 	if len(out) < 2 {
 		fmt.Fprintln(os.Stderr, "No changes found")
 	} else if len(file) > 0 {
 		path := new.GetMigrationPath(file)
-		return afero.WriteFile(fsys, path, []byte(out), 0644)
+		if err := afero.WriteFile(fsys, path, []byte(out), 0644); err != nil {
+			return err
+		}
+		fmt.Fprintln(os.Stderr, warnDiff)
 	} else {
 		fmt.Println(out)
 	}
 	return nil
 }
 
-// TODO: Handle cleanup on SIGINT/SIGTERM.
-func Run(file string, fsys afero.Fs) error {
+func Run(ctx context.Context, schema []string, file, password string, fsys afero.Fs) error {
 	// Sanity checks.
 	{
 		if err := utils.LoadConfigFS(fsys); err != nil {
@@ -48,199 +40,54 @@ func Run(file string, fsys afero.Fs) error {
 		}
 	}
 
-	s := spinner.NewModel()
-	s.Spinner = spinner.Dot
-	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
-	p := utils.NewProgram(model{spinner: s})
-
-	errCh := make(chan error, 1)
-	go func() {
-		errCh <- run(p)
-		p.Send(tea.Quit())
-	}()
-
-	if err := p.Start(); err != nil {
-		return err
-	}
-	if errors.Is(ctx.Err(), context.Canceled) {
-		return errors.New("Aborted " + utils.Aqua("supabase db diff") + ".")
-	}
-	if err := <-errCh; err != nil {
+	if err := utils.RunProgram(ctx, func(p utils.Program, ctx context.Context) error {
+		return run(p, ctx, schema, fsys)
+	}); err != nil {
 		return err
 	}
 
-	return SaveDiff(diff, file, fsys)
+	return SaveDiff(output, file, fsys)
 }
 
-var (
-	ctx, cancelCtx = context.WithCancel(context.Background())
-	diff           string
-)
+var output string
 
-func run(p utils.Program) error {
+func run(p utils.Program, ctx context.Context, schema []string, fsys afero.Fs) error {
 	p.Send(utils.StatusMsg("Creating shadow database..."))
 
 	// 1. Create shadow db and run migrations
-	{
-		if err := commit.ResetDatabase(ctx, utils.DbId, utils.ShadowDbName); err != nil {
-			return err
-		}
-
-		if err := utils.MkdirIfNotExist("supabase/migrations"); err != nil {
-			return err
-		}
-		migrations, err := os.ReadDir("supabase/migrations")
-		if err != nil {
-			return err
-		}
-
-		for i, migration := range migrations {
-			// NOTE: To handle backward-compatibility. `<timestamp>_init.sql` as
-			// the first migration (prev versions of the CLI) is deprecated.
-			if i == 0 {
-				matches := regexp.MustCompile(`([0-9]{14})_init\.sql`).FindStringSubmatch(migration.Name())
-				if len(matches) == 2 {
-					if timestamp, err := strconv.ParseUint(matches[1], 10, 64); err != nil {
-						return err
-					} else if timestamp < 20211209000000 {
-						continue
-					}
-				}
-			}
-
-			p.Send(utils.StatusMsg("Applying migration " + utils.Bold(migration.Name()) + "..."))
-
-			content, err := os.ReadFile("supabase/migrations/" + migration.Name())
-			if err != nil {
-				return err
-			}
-
-			out, err := utils.DockerExec(ctx, utils.DbId, []string{
-				"sh", "-c", `PGOPTIONS='--client-min-messages=error' psql postgresql://postgres:postgres@localhost/` + utils.ShadowDbName + ` <<'EOSQL'
-BEGIN;
-` + string(content) + `
-COMMIT;
-EOSQL
-`,
-			})
-			if err != nil {
-				return err
-			}
-			var errBuf bytes.Buffer
-			if _, err := stdcopy.StdCopy(io.Discard, &errBuf, out); err != nil {
-				return err
-			}
-			if errBuf.Len() > 0 {
-				return errors.New("Error starting shadow database: " + errBuf.String())
-			}
-		}
+	shadow, err := CreateShadowDatabase(ctx)
+	if err != nil {
+		return err
+	}
+	defer utils.DockerRemove(shadow)
+	if err := MigrateShadowDatabase(ctx, fsys); err != nil {
+		return err
 	}
 
 	p.Send(utils.StatusMsg("Diffing local database with current migrations..."))
 
 	// 2. Diff local db (source) with shadow db (target), print it.
-	{
-		out, err := utils.DockerExec(ctx, utils.DifferId, []string{
-			"sh", "-c", "/venv/bin/python3 -u cli.py --json-diff" +
-				" 'postgresql://postgres:postgres@" + utils.DbId + ":5432/postgres'" +
-				" 'postgresql://postgres:postgres@" + utils.DbId + ":5432/" + utils.ShadowDbName + "'",
-		})
-		if err != nil {
-			return err
-		}
-
-		diffBytes, err := utils.ProcessDiffOutput(p, out)
-		if err != nil {
-			return err
-		}
-
-		diff = string(diffBytes)
-	}
-
-	return nil
+	source := "postgresql://postgres:postgres@" + utils.DbId + ":5432/postgres"
+	target := "postgresql://postgres:postgres@" + shadow[:12] + ":5432/postgres"
+	output, err = DiffSchema(ctx, source, target, schema, p)
+	return err
 }
 
-type model struct {
-	spinner     spinner.Model
-	status      string
-	progress    *progress.Model
-	psqlOutputs []string
-
-	width int
-}
-
-func (m model) Init() tea.Cmd {
-	return spinner.Tick
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyCtrlC:
-			// Stop future runs
-			cancelCtx()
-			return m, tea.Quit
-		default:
-			return m, nil
+func DiffSchema(ctx context.Context, source, target string, schema []string, p utils.Program) (string, error) {
+	stream := utils.NewDiffStream(p)
+	for _, s := range schema {
+		p.Send(utils.StatusMsg("Diffing schema: " + s))
+		if err := utils.DockerRunOnceWithStream(
+			ctx,
+			utils.DifferImage,
+			nil,
+			[]string{"--schema", s, "--json-diff", source, target},
+			stream.Stdout(),
+			stream.Stderr(),
+		); err != nil {
+			return "", err
 		}
-	case tea.WindowSizeMsg:
-		m.width = msg.Width
-		return m, nil
-	case spinner.TickMsg:
-		spinnerModel, cmd := m.spinner.Update(msg)
-		m.spinner = spinnerModel
-		return m, cmd
-	case progress.FrameMsg:
-		if m.progress == nil {
-			return m, nil
-		}
-
-		tmp, cmd := m.progress.Update(msg)
-		progressModel := tmp.(progress.Model)
-		m.progress = &progressModel
-		return m, cmd
-	case utils.StatusMsg:
-		m.status = string(msg)
-		return m, nil
-	case utils.ProgressMsg:
-		if msg == nil {
-			m.progress = nil
-			return m, nil
-		}
-
-		if m.progress == nil {
-			progressModel := progress.NewModel(progress.WithGradient("#1c1c1c", "#34b27b"))
-			m.progress = &progressModel
-		}
-
-		return m, m.progress.SetPercent(*msg)
-	case utils.PsqlMsg:
-		if msg == nil {
-			m.psqlOutputs = []string{}
-			return m, nil
-		}
-
-		m.psqlOutputs = append(m.psqlOutputs, *msg)
-		if len(m.psqlOutputs) > 5 {
-			m.psqlOutputs = m.psqlOutputs[1:]
-		}
-		return m, nil
-	default:
-		return m, nil
 	}
-}
-
-func (m model) View() string {
-	var progress string
-	if m.progress != nil {
-		progress = "\n\n" + m.progress.View()
-	}
-
-	var psqlOutputs string
-	if len(m.psqlOutputs) > 0 {
-		psqlOutputs = "\n\n" + strings.Join(m.psqlOutputs, "\n")
-	}
-
-	return wrap.String(m.spinner.View()+m.status+progress+psqlOutputs, m.width)
+	diffBytes, err := stream.Collect()
+	return string(diffBytes), err
 }
