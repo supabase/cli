e596820b98204a2fa2c94a28621a741f81bed982
Qiao Han <qiao@supabase.io>
2023-01-03 15:06:26 +0800

    fix: additional flags for diffing extensions

--- internal/db/diff/templates/migra.sh
+++ internal/db/diff/templates/migra.sh
@@ -7,10 +7,16 @@ pip install -qU migra==3.0.1663481299
 # migra doesn't shutdown gracefully, so kill it ourselves
 trap 'kill -9 %1' TERM
 
+run_migra() {
+    # additional flags for diffing extensions
+    [ "$schema" = "extensions" ] && set -- --create-extensions-only --ignore-extension-versions "$@"
+    migra --unsafe --schema="$schema" "$@"
+}
+
 # accepts command line args as a list of schema to generate
-for i in "$@"; do
+for schema in "$@"; do
     # migra exits 2 when differences are found
-    migra --unsafe --schema="$i" "$SOURCE" "$TARGET" || status=$?
+    run_migra "$SOURCE" "$TARGET" || status=$?
     if [ ${status:-2} -ne 2 ]; then
         exit $status
     fi
