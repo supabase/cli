c934dd207358603afa53518425534922bdcea667
Qiao Han <qiao@supabase.io>
2023-01-03 15:07:25 +0800

    fix: initial storage schema indentation

--- internal/utils/templates/initial_schemas/14.sql
+++ internal/utils/templates/initial_schemas/14.sql
@@ -651,10 +651,10 @@ DECLARE
 _parts text[];
 _filename text;
 BEGIN
-    select string_to_array(name, '/') into _parts;
-    select _parts[array_length(_parts,1)] into _filename;
-    -- @todo return the last part instead of 2
-    return split_part(_filename, '.', 2);
+	select string_to_array(name, '/') into _parts;
+	select _parts[array_length(_parts,1)] into _filename;
+	-- @todo return the last part instead of 2
+	return split_part(_filename, '.', 2);
 END
 $$;
 
@@ -671,8 +671,8 @@ CREATE OR REPLACE FUNCTION storage.filename(name text) RETURNS text
 DECLARE
 _parts text[];
 BEGIN
-    select string_to_array(name, '/') into _parts;
-    return _parts[array_length(_parts,1)];
+	select string_to_array(name, '/') into _parts;
+	return _parts[array_length(_parts,1)];
 END
 $$;
 
@@ -689,8 +689,8 @@ CREATE OR REPLACE FUNCTION storage.foldername(name text) RETURNS text[]
 DECLARE
 _parts text[];
 BEGIN
-    select string_to_array(name, '/') into _parts;
-    return _parts[1:array_length(_parts,1)-1];
+	select string_to_array(name, '/') into _parts;
+	return _parts[1:array_length(_parts,1)-1];
 END
 $$;
 
