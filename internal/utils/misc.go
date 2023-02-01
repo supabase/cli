1140dce36ebf2304702baaebaa9e8163fb072777
Lakshan Perera <lakshan@laktek.com>
2023-02-01 12:32:39 +0700

    fix: use edge-runtime 1.0.7 image

--- internal/utils/misc.go
+++ internal/utils/misc.go
@@ -37,7 +37,7 @@ const (
 	StudioImage      = "supabase/studio:20230127-6bfd87b"
 	DenoRelayImage   = "supabase/deno-relay:v1.5.0"
 	ImageProxyImage  = "darthsim/imgproxy:v3.8.0"
-	EdgeRuntimeImage = "supabase/edge-runtime:latest"
+	EdgeRuntimeImage = "supabase/edge-runtime:v1.0.7"
 	// Update initial schemas in internal/utils/templates/initial_schemas when
 	// updating any one of these.
 	GotrueImage   = "supabase/gotrue:v2.40.1"
