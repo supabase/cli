ec1ddb10a2b175dda0fc94dff17961a171f687ee
Qiao Han <qiao@supabase.io>
2023-02-17 22:18:29 +0800

    fix: bump postgres image to latest slim

--- internal/utils/misc.go
+++ internal/utils/misc.go
@@ -29,7 +29,7 @@ var Version string
 const (
 	Pg13Image = "supabase/postgres:13.3.0"
 	Pg14Image = "supabase/postgres:14.1.0.89"
-	Pg15Image = "supabase/postgres:15.1.0.33"
+	Pg15Image = "supabase/postgres:15.1.0.42-rc2"
 	// Append to ServiceImages when adding new dependencies below
 	KongImage        = "library/kong:2.8.1"
 	InbucketImage    = "inbucket/inbucket:3.0.3"
