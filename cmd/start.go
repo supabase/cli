b4ce32ff6120c6ad44e16c4415ed39a1f947a856
Zach Blume <zachblume@gmail.com>
2023-03-06 21:01:37 -0500

    Update start.go

--- cmd/start.go
+++ cmd/start.go
@@ -26,7 +26,7 @@ var (
 func init() {
 	flags := startCmd.Flags()
 	names := strings.Join(allowedContainers, ", ")
-	flags.StringSliceVarP(&excludedContainers, "exclude", "x", []string{}, "Names of containers to not start. ["+names+"]")
+	flags.StringSliceVarP(&excludedContainers, "exclude", "x", []string{}, "Names of containers to not start. Multiple excludes should be comma seperated, without spaces, such as `-x studio,imgproxy`. ["+names+"]")
 	flags.BoolVar(&ignoreHealthCheck, "ignore-health-check", false, "Ignore unhealthy services and exit 0")
 	rootCmd.AddCommand(startCmd)
 }
