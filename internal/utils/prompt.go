8bac26c02c4dec7bcdec1ee7d1a2635da0c600b4
Qiao Han <qiao@supabase.io>
2023-03-23 22:30:26 +0800

    chore: update item renderer to accept multiple values

--- internal/utils/prompt.go
+++ internal/utils/prompt.go
@@ -49,8 +49,9 @@ func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list
 
 	fn := itemStyle.Render
 	if index == m.Index() {
-		fn = func(s string) string {
-			return selectedItemStyle.Render("> " + s)
+		fn = func(s ...string) string {
+			items := append([]string{"> "}, s...)
+			return selectedItemStyle.Render(items...)
 		}
 	}
 
