fc6cc11f47dac332879423ae01997f3d49f4c528
Qiao Han <qiao@supabase.io>
2023-03-26 16:25:06 +0800

    fix: reset database as supabase admin

--- internal/utils/templates/initial_schemas/14.sql
+++ internal/utils/templates/initial_schemas/14.sql
@@ -3437,7 +3437,7 @@ CREATE EVENT TRIGGER issue_pg_cron_access ON ddl_command_end
    EXECUTE FUNCTION extensions.grant_pg_cron_access();
 
 
-ALTER EVENT TRIGGER issue_pg_cron_access OWNER TO postgres;
+ALTER EVENT TRIGGER issue_pg_cron_access OWNER TO supabase_admin;
 
 --
 -- Name: issue_pg_graphql_access; Type: EVENT TRIGGER; Schema: -; Owner: supabase_admin
@@ -3459,7 +3459,7 @@ CREATE EVENT TRIGGER issue_pg_net_access ON ddl_command_end
    EXECUTE FUNCTION extensions.grant_pg_net_access();
 
 
-ALTER EVENT TRIGGER issue_pg_net_access OWNER TO postgres;
+ALTER EVENT TRIGGER issue_pg_net_access OWNER TO supabase_admin;
 
 --
 -- Name: pgrst_ddl_watch; Type: EVENT TRIGGER; Schema: -; Owner: supabase_admin
