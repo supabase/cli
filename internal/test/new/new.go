f10804551b9c48b560472be374a6ed8e0876dc95
Qiao Han <qiao@supabase.io>
2023-04-25 18:14:27 +0800

    feat: implement test new command

---
+++ internal/test/new/new.go
@@ -0,0 +1,28 @@
+package new
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"path/filepath"
+
+	"github.com/spf13/afero"
+	"github.com/supabase/cli/internal/utils"
+)
+
+var (
+	//go:embed templates/pgtap.sql
+	pgtapTest []byte
+)
+
+func Run(ctx context.Context, name string, fsys afero.Fs) error {
+	path := filepath.Join(utils.DbTestsDir, fmt.Sprintf("%s_test.sql", name))
+	if err := utils.MkdirIfNotExistFS(fsys, filepath.Dir(path)); err != nil {
+		return err
+	}
+	err := afero.WriteFile(fsys, path, pgtapTest, 0644)
+	if err == nil {
+		fmt.Println("Created new pgTAP test at " + utils.Bold(path) + ".")
+	}
+	return err
+}
