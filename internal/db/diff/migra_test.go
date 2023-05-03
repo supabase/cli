0d34fb4b802d78d8fac884d5e856f56436476277
Qiao Han <qiao@supabase.io>
2023-05-04 01:42:28 +0800

    chore: cleanup unused migration function

--- internal/db/diff/migra_test.go
+++ internal/db/diff/migra_test.go
@@ -15,7 +15,6 @@ import (
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/supabase/cli/internal/migration/repair"
 	"github.com/supabase/cli/internal/testing/apitest"
 	"github.com/supabase/cli/internal/testing/pgtest"
 	"github.com/supabase/cli/internal/utils"
@@ -131,66 +130,6 @@ func TestRunMigra(t *testing.T) {
 		// Check error
 		assert.ErrorContains(t, err, "network error")
 		assert.Empty(t, apitest.ListUnmatchedRequests())
-	})
-}
-
-func TestApplyMigrations(t *testing.T) {
-	const postgresUrl = "postgresql://postgres:password@localhost:5432/postgres"
-
-	t.Run("applies migrations from local directory", func(t *testing.T) {
-		// Setup in-memory fs
-		fsys := afero.NewMemMapFs()
-		// Setup initial migration
-		migrations := map[string]string{
-			filepath.Join(utils.MigrationsDir, "20220727064247_init.sql"): "create table test",
-			filepath.Join(utils.MigrationsDir, "20220727064248_drop.sql"): "drop table test;\n-- ignore me",
-		}
-		for path, query := range migrations {
-			require.NoError(t, afero.WriteFile(fsys, path, []byte(query), 0644))
-		}
-		// Setup mock postgres
-		conn := pgtest.NewConn()
-		defer conn.Close(t)
-		conn.Query("create table test").
-			Reply("SELECT 0").
-			Query(repair.INSERT_MIGRATION_VERSION, "20220727064247").
-			Reply("INSERT 1").
-			Query("drop table test").
-			Reply("SELECT 0").
-			Query("-- ignore me").
-			Reply("").
-			Query(repair.INSERT_MIGRATION_VERSION, "20220727064248").
-			Reply("INSERT 1")
-		// Run test
-		assert.NoError(t, ApplyMigrations(context.Background(), postgresUrl, fsys, conn.Intercept))
-	})
-
-	t.Run("throws error on invalid postgres url", func(t *testing.T) {
-		assert.Error(t, ApplyMigrations(context.Background(), "invalid", afero.NewMemMapFs()))
-	})
-
-	t.Run("throws error on failture to connect", func(t *testing.T) {
-		assert.Error(t, ApplyMigrations(context.Background(), postgresUrl, afero.NewMemMapFs()))
-	})
-
-	t.Run("throws error on failture to send batch", func(t *testing.T) {
-		// Setup in-memory fs
-		fsys := afero.NewMemMapFs()
-		// Setup initial migration
-		name := "20220727064247_create_table.sql"
-		path := filepath.Join(utils.MigrationsDir, name)
-		query := "create table test"
-		require.NoError(t, afero.WriteFile(fsys, path, []byte(query), 0644))
-		// Setup mock postgres
-		conn := pgtest.NewConn()
-		defer conn.Close(t)
-		conn.Query(query).
-			ReplyError(pgerrcode.DuplicateTable, `relation "test" already exists`).
-			Query(repair.INSERT_MIGRATION_VERSION, "20220727064247")
-		// Run test
-		err := ApplyMigrations(context.Background(), postgresUrl, fsys, conn.Intercept)
-		// Check error
-		assert.ErrorContains(t, err, "ERROR: relation \"test\" already exists (SQLSTATE 42P07)\nAt statement 0: create table test")
 	})
 }
 
