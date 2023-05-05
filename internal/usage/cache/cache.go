84ac434a45a3146bf86bf36ba6c6d7f688ed5d70
Qiao Han <qiao@supabase.io>
2023-05-05 17:43:16 +0800

    feat: implement usage commands for db stats

---
+++ internal/usage/cache/cache.go
@@ -0,0 +1,52 @@
+package cache
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
+	"github.com/spf13/afero"
+	"github.com/supabase/cli/internal/migration/list"
+	"github.com/supabase/cli/internal/utils"
+	"github.com/supabase/cli/internal/utils/pgxv5"
+)
+
+// Ref: https://github.com/heroku/heroku-pg-extras/blob/main/commands/cache_hit.js#L7
+const CACHE_HIT_QUERY = `
+SELECT
+  'index hit rate' AS name,
+  (sum(idx_blks_hit)) / nullif(sum(idx_blks_hit + idx_blks_read),0) AS ratio
+FROM pg_statio_user_indexes
+UNION ALL
+SELECT
+ 'table hit rate' AS name,
+  sum(heap_blks_hit) / nullif(sum(heap_blks_hit) + sum(heap_blks_read),0) AS ratio
+FROM pg_statio_user_tables;
+`
+
+type CacheHitResult struct {
+	Name  string
+	Ratio float64
+}
+
+func Run(ctx context.Context, config pgconn.Config, fsys afero.Fs, options ...func(*pgx.ConnConfig)) error {
+	conn, err := utils.ConnectRemotePostgres(ctx, config, options...)
+	if err != nil {
+		return err
+	}
+	rows, err := conn.Query(ctx, CACHE_HIT_QUERY)
+	if err != nil {
+		return err
+	}
+	result, err := pgxv5.CollectRows[CacheHitResult](rows)
+	if err != nil {
+		return err
+	}
+	// TODO: implement a markdown table marshaller
+	table := "|Name|Ratio|\n|-|-|\n"
+	for _, r := range result {
+		table += fmt.Sprintf("|`%s`|`%.6f`|\n", r.Name, r.Ratio)
+	}
+	return list.RenderTable(table)
+}
