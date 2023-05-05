ea169f90583bd7d11478bbddde8eebfe40595c45
Han Qiao <sweatybridge@gmail.com>
2023-05-05 12:03:00 +0800

    fix: bump studio image to latest

--- internal/utils/misc.go
+++ internal/utils/misc.go
@@ -30,7 +30,7 @@ const (
 	DifferImage      = "supabase/pgadmin-schema-diff:cli-0.0.5"
 	MigraImage       = "djrobstep/migra:3.0.1621480950"
 	PgmetaImage      = "supabase/postgres-meta:v0.60.7"
-	StudioImage      = "supabase/studio:20230428-6ff285f"
+	StudioImage      = "supabase/studio:20230504-0f52967"
 	DenoRelayImage   = "supabase/deno-relay:v1.6.0"
 	ImageProxyImage  = "darthsim/imgproxy:v3.8.0"
 	EdgeRuntimeImage = "supabase/edge-runtime:v1.2.18"
