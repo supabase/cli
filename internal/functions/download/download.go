386219392264892ec6c2bec6433e8ae6fb485cb4
Qiao Han <qiao@supabase.io>
2023-05-16 22:58:20 +0800

    chore: add unit tests for function download

--- internal/functions/download/download.go
+++ internal/functions/download/download.go
@@ -37,43 +37,8 @@ func Run(ctx context.Context, slug string, projectRef string, fsys afero.Fs) err
 	}
 
 	// 2. Download Function.
-	{
-		fmt.Println("Downloading " + utils.Bold(slug))
-		denoPath, err := utils.GetDenoPath()
-		if err != nil {
-			return err
-		}
-
-		meta, err := getFunctionMetadata(ctx, projectRef, slug)
-		if err != nil {
-			return err
-		}
-
-		resp, err := utils.GetSupabase().GetFunctionBodyWithResponse(ctx, projectRef, slug)
-		if err != nil {
-			return err
-		}
-
-		switch resp.StatusCode() {
-		case http.StatusNotFound: // Function doesn't exist
-			return errors.New("Function " + utils.Aqua(slug) + " does not exist on the Supabase project.")
-		case http.StatusOK: // Function exists
-			resBuf := bytes.NewReader(resp.Body)
-
-			extractScriptPath := scriptDir.ExtractPath
-			funcDir := filepath.Join(utils.FunctionsDir, slug)
-			var errBuf bytes.Buffer
-			args := []string{"run", "-A", extractScriptPath, funcDir, *meta.EntrypointPath}
-			cmd := exec.CommandContext(ctx, denoPath, args...)
-			cmd.Stdin = resBuf
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = &errBuf
-			if err := cmd.Run(); err != nil {
-				return fmt.Errorf("Error downloading function: %w\n%v", err, errBuf.String())
-			}
-		default:
-			return errors.New("Unexpected error downloading Function: " + string(resp.Body))
-		}
+	if err := downloadFunction(ctx, projectRef, slug, scriptDir.ExtractPath); err != nil {
+		return err
 	}
 
 	fmt.Println("Downloaded Function " + utils.Aqua(slug) + " from project " + utils.Aqua(projectRef) + ".")
@@ -85,7 +50,13 @@ func getFunctionMetadata(ctx context.Context, projectRef, slug string) (*api.Fun
 	if err != nil {
 		return nil, err
 	}
-	if resp.JSON200 == nil {
+
+	switch resp.StatusCode() {
+	case http.StatusNotFound:
+		return nil, errors.New("Function " + utils.Aqua(slug) + " does not exist on the Supabase project.")
+	case http.StatusOK:
+		break
+	default:
 		return nil, errors.New("Failed to download Function " + utils.Aqua(slug) + " on the Supabase project: " + string(resp.Body))
 	}
 
@@ -96,4 +67,38 @@ func getFunctionMetadata(ctx context.Context, projectRef, slug string) (*api.Fun
 		resp.JSON200.ImportMapPath = &legacyImportMapPath
 	}
 	return resp.JSON200, nil
+}
+
+func downloadFunction(ctx context.Context, projectRef, slug, extractScriptPath string) error {
+	fmt.Println("Downloading " + utils.Bold(slug))
+	denoPath, err := utils.GetDenoPath()
+	if err != nil {
+		return err
+	}
+
+	meta, err := getFunctionMetadata(ctx, projectRef, slug)
+	if err != nil {
+		return err
+	}
+
+	resp, err := utils.GetSupabase().GetFunctionBodyWithResponse(ctx, projectRef, slug)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode() != http.StatusOK {
+		return errors.New("Unexpected error downloading Function: " + string(resp.Body))
+	}
+
+	resBuf := bytes.NewReader(resp.Body)
+	funcDir := filepath.Join(utils.FunctionsDir, slug)
+	args := []string{"run", "-A", extractScriptPath, funcDir, *meta.EntrypointPath}
+	cmd := exec.CommandContext(ctx, denoPath, args...)
+	var errBuf bytes.Buffer
+	cmd.Stdin = resBuf
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = &errBuf
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("Error downloading function: %w\n%v", err, errBuf.String())
+	}
+	return nil
 }
