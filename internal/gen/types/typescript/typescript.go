21f01aab8f3d70d058cb1c720cae885e422d2a2d
Bobbie Soedirgo <bobbie@soedirgo.dev>
2023-05-30 12:03:49 +0800

    fix(typegen): wrong pg-meta file on `--db-url`

--- internal/gen/types/typescript/typescript.go
+++ internal/gen/types/typescript/typescript.go
@@ -68,7 +68,7 @@ func Run(ctx context.Context, useLocal bool, useLinked bool, projectId string, d
 				},
 				Cmd: []string{
 					"node",
-					"dist/server/app.js",
+					"dist/server/server.js",
 					"gen",
 					"types",
 					"typescript",
