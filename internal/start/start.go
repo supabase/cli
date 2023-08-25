0132a7ddcc70daa9a253281261bf77c38b60b413
Han Qiao <sweatybridge@gmail.com>
2023-08-25 15:56:20 +0800

    Revert "fix: add 10 seconds startup time for studio (#1387)"
    This reverts commit 59b71ba3bc473d3f0e07a546cb2bb9e5a5acf1d0.

--- internal/start/start.go
+++ internal/start/start.go
@@ -734,11 +734,10 @@ EOF
 					fmt.Sprintf("NEXT_ANALYTICS_BACKEND_PROVIDER=%v", utils.Config.Analytics.Backend),
 				},
 				Healthcheck: &container.HealthConfig{
-					Test:        []string{"CMD", "node", "-e", "require('http').get('http://localhost:3000/api/profile', (r) => {if (r.statusCode !== 200) throw new Error(r.statusCode)})"},
-					Interval:    2 * time.Second,
-					Timeout:     2 * time.Second,
-					Retries:     10,
-					StartPeriod: 10 * time.Second,
+					Test:     []string{"CMD", "node", "-e", "require('http').get('http://localhost:3000/api/profile', (r) => {if (r.statusCode !== 200) throw new Error(r.statusCode)})"},
+					Interval: 2 * time.Second,
+					Timeout:  2 * time.Second,
+					Retries:  10,
 				},
 			},
 			container.HostConfig{
