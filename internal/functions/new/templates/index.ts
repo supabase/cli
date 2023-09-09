2f2fa3ba1a1a7b04a167b69c70612776d8f843d3
Adrien Maret <amaret93@gmail.com>
2023-09-09 23:31:51 +0200

    Update new function template
    Update the CURL command used to invoke the function.
    
    Reference documentation https://supabase.com/docs/guides/functions/quickstart#invoking-remote-functions

--- internal/functions/new/templates/index.ts
+++ internal/functions/new/templates/index.ts
@@ -19,7 +19,8 @@ serve(async (req) => {
 })
 
 // To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+// curl -i --location --request POST 'https://<project reference>.functions.supabase.co/<function name>' \
+//   --header 'Authorization: Bearer <jwt token>' \
 //   --header 'Content-Type: application/json' \
 //   --data '{"name":"Functions"}'
+
