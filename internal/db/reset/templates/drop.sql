3a8443c96a9c216d1ef8fc6a492576f1b39269ca
Han Qiao <sweatybridge@gmail.com>
2023-09-14 14:13:12 +0800

    fix(db): drop non-default extensions when resetting remote (#1490)

--- internal/db/reset/templates/drop.sql
+++ internal/db/reset/templates/drop.sql
@@ -1,6 +1,15 @@
 do $$ declare
   rec record;
 begin
+  -- extensions
+  for rec in
+    select *
+    from pg_extension p
+    where p.extname not in ('pg_graphql', 'pg_net', 'pg_stat_statements', 'pgcrypto', 'pgjwt', 'pgsodium', 'plpgsql', 'supabase_vault', 'uuid-ossp')
+  loop
+    execute format('drop extension if exists %I cascade', rec.extname);
+  end loop;
+
   -- functions
   for rec in
     select *
@@ -11,17 +20,28 @@ begin
     execute format('drop routine if exists %I.%I(%s) cascade', rec.pronamespace::regnamespace::name, rec.proname, pg_catalog.pg_get_function_identity_arguments(rec.oid));
   end loop;
 
-  -- in order: tables (cascade to views), sequences
+  -- tables (cascade to views)
   for rec in
     select *
     from pg_class c
     where
       c.relnamespace::regnamespace::name = 'public'
-      and c.relkind not in ('c', 'v', 'm')
+      and c.relkind not in ('c', 'S', 'v', 'm')
     order by c.relkind desc
   loop
-    -- supports all kinds of relations, except views and complex types
+    -- supports all table like relations, except views, complex types, and sequences
     execute format('drop table if exists %I.%I cascade', rec.relnamespace::regnamespace::name, rec.relname);
+  end loop;
+
+  -- sequences
+  for rec in
+    select *
+    from pg_class c
+    where
+      c.relnamespace::regnamespace::name = 'public'
+      and c.relkind = 's'
+  loop
+    execute format('drop sequence if exists %I.%I cascade', rec.relnamespace::regnamespace::name, rec.relname);
   end loop;
 
   -- types
