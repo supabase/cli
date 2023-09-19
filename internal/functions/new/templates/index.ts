dd5d5c04fe5c13a08ff7d1a234efd15c88af9e4d
Ofir Levitan <eifr0980@gmail.com>
2023-09-19 09:56:28 +0300

    Update index.ts

--- internal/functions/new/templates/index.ts
+++ internal/functions/new/templates/index.ts
@@ -2,11 +2,9 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
-
 console.log("Hello from Functions!")
 
-serve(async (req) => {
+Deno.serve(async (req) => {
   const { name } = await req.json()
   const data = {
     message: `Hello ${name}!`,
