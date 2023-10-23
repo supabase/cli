282395c572ed65108846da0ec3053107c9f010c1
Qiao Han <qiao@supabase.io>
2023-10-23 18:54:13 +0800

    fix(studio): update image version to 20231023

--- internal/utils/misc.go
+++ internal/utils/misc.go
@@ -31,7 +31,7 @@ const (
 	DifferImage      = "supabase/pgadmin-schema-diff:cli-0.0.5"
 	MigraImage       = "djrobstep/migra:3.0.1621480950"
 	PgmetaImage      = "supabase/postgres-meta:v0.68.0"
-	StudioImage      = "supabase/studio:20230921-d657f29"
+	StudioImage      = "supabase/studio:20231023-7e2cd92"
 	ImageProxyImage  = "darthsim/imgproxy:v3.8.0"
 	EdgeRuntimeImage = "supabase/edge-runtime:v1.20.2"
 	VectorImage      = "timberio/vector:0.28.1-alpine"
