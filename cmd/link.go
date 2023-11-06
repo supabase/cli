c355dc131d5edc189b23a23c84444b33a5e30809
Qiao Han <qiao@supabase.io>
2023-11-06 12:46:37 +0800

    feat: prompt for project ref in terminal environments

--- cmd/link.go
+++ cmd/link.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"os"
 	"os/signal"
 
@@ -8,6 +11,8 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/supabase/cli/internal/link"
+	"github.com/supabase/cli/internal/utils"
+	"golang.org/x/term"
 )
 
 var (
@@ -17,7 +22,19 @@ var (
 		GroupID: groupLocalDev,
 		Use:     "link",
 		Short:   "Link to a Supabase project",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if !term.IsTerminal(int(os.Stdin.Fd())) {
+				return cmd.MarkFlagRequired("project-ref")
+			}
+			return nil
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx, _ := signal.NotifyContext(cmd.Context(), os.Interrupt)
+			if len(projectRef) == 0 {
+				if err := PromptProjectRef(ctx); err != nil {
+					return err
+				}
+			}
 			fsys := afero.NewOsFs()
 			if err := link.PreRun(projectRef, fsys); err != nil {
 				return err
@@ -26,7 +43,6 @@ var (
 			if dbPassword == "" {
 				dbPassword = link.PromptPasswordAllowBlank(os.Stdin)
 			}
-			ctx, _ := signal.NotifyContext(cmd.Context(), os.Interrupt)
 			return link.Run(ctx, projectRef, dbPassword, fsys)
 		},
 		PostRunE: func(cmd *cobra.Command, args []string) error {
@@ -40,6 +56,27 @@ func init() {
 	flags.StringVar(&projectRef, "project-ref", "", "Project ref of the Supabase project.")
 	flags.StringVarP(&dbPassword, "password", "p", "", "Password to your remote Postgres database.")
 	cobra.CheckErr(viper.BindPFlag("DB_PASSWORD", flags.Lookup("password")))
-	cobra.CheckErr(linkCmd.MarkFlagRequired("project-ref"))
 	rootCmd.AddCommand(linkCmd)
+}
+
+func PromptProjectRef(ctx context.Context) error {
+	title := "Which project do you want to link?"
+	resp, err := utils.GetSupabase().GetProjectsWithResponse(ctx)
+	if err != nil {
+		return err
+	}
+	if resp.JSON200 == nil {
+		return errors.New("Unexpected error retrieving projects: " + string(resp.Body))
+	}
+	items := make([]utils.PromptItem, len(*resp.JSON200))
+	for i, project := range *resp.JSON200 {
+		items[i] = utils.PromptItem{Summary: project.Name, Details: project.Id}
+	}
+	choice, err := utils.PromptChoice(ctx, title, items)
+	if err != nil {
+		return err
+	}
+	projectRef = choice.Details
+	fmt.Fprintln(os.Stderr, "Selected project ref:", projectRef)
+	return nil
 }
