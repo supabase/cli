f32ada8437db72f321a3766e768009ad293e3c79
Lakshan Perera <lakshan@laktek.com>
2023-12-05 18:02:39 +1100

    fix: TempDir call

--- internal/functions/deploy/deploy.go
+++ internal/functions/deploy/deploy.go
@@ -75,7 +75,7 @@ func bundleFunction(ctx context.Context, dockerEntrypointPath, importMapPath str
 	}
 
 	// create temp directory to store generated eszip
-	tmpDir, err := afero.TempDir(fsys, "eszip")
+	tmpDir, err := afero.TempDir(fsys, "", "eszip")
 	if err != nil {
 		return nil, err
 	}
