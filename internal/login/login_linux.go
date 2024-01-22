dc21dd7f2d5eb2e2a1e90991f155e9ec2b8d13d4
JongChan Choi (Rieul) <disjukr@naver.com>
2024-01-22 17:30:08 +0900

    fix: make `supabase login` works on wsl (#1866)
    * fix: make `supabase login` works on wsl
    
    * Update internal/login/login_linux.go
    
    Co-authored-by: Han Qiao <sweatybridge@gmail.com>
    
    * format
    
    ---------
    
    Co-authored-by: Han Qiao <sweatybridge@gmail.com>

---
+++ internal/login/login_linux.go
@@ -0,0 +1,17 @@
+//go:build !windows && !darwin
+
+package login
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"os/exec"
+)
+
+func RunOpenCmd(ctx context.Context, input string) error {
+	if f, err := os.ReadFile("/proc/sys/kernel/osrelease"); err == nil && bytes.Contains(f, []byte("WSL")) {
+		return exec.CommandContext(ctx, "wslview", input).Run()
+	}
+	return exec.CommandContext(ctx, "xdg-open", input).Run()
+}
