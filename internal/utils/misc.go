c72e03a3b70bc84a4507b57e7ded20732deac763
Fabrizio <fabri.feno@gmail.com>
2024-01-29 03:58:12 +0000

    fix(storage): bump storage version to v0.46.4 (#1883)
    * fix: bump storage v0.46.3
    
    * chore: resolve lint error
    
    * chore: bump version
    
    ---------
    
    Co-authored-by: Qiao Han <qiao@supabase.io>
    Co-authored-by: Han Qiao <sweatybridge@gmail.com>

--- internal/utils/misc.go
+++ internal/utils/misc.go
@@ -40,7 +40,7 @@ const (
 	PgProveImage     = "supabase/pg_prove:3.36"
 	GotrueImage      = "supabase/gotrue:v2.132.3"
 	RealtimeImage    = "supabase/realtime:v2.25.50"
-	StorageImage     = "supabase/storage-api:v0.43.11"
+	StorageImage     = "supabase/storage-api:v0.46.4"
 	LogflareImage    = "supabase/logflare:1.4.0"
 	// Should be kept in-sync with EdgeRuntimeImage
 	DenoVersion = "1.30.3"
