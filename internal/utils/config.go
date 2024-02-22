361ecb3d964b2692397b7762776f9492103146bd
Felix Gabler <felix.gabler98@gmail.com>
2024-02-22 08:24:37 +0000

    feat: allow users to specify OpenAI API key for studio

--- internal/utils/config.go
+++ internal/utils/config.go
@@ -286,9 +286,10 @@ type (
 	}
 
 	studio struct {
-		Enabled bool   `toml:"enabled"`
-		Port    uint   `toml:"port"`
-		ApiUrl  string `toml:"api_url"`
+		Enabled 	 bool   `toml:"enabled"`
+		Port    	 uint   `toml:"port"`
+		ApiUrl  	 string `toml:"api_url"`
+		OpenaiApiKey string `toml:"openai_api_key"`
 	}
 
 	inbucket struct {
