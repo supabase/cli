6e4cf16ee9eb6da230867b8c76864f5dda61ba82
Qiao Han <qiao@supabase.io>
2024-03-13 15:49:27 +0800

    fix: disable keyring when exec binary is not found

--- internal/utils/credentials/store.go
+++ internal/utils/credentials/store.go
@@ -3,6 +3,7 @@ package credentials
 import (
 	"bytes"
 	"os"
+	"os/exec"
 
 	"github.com/go-errors/errors"
 	"github.com/zalando/go-keyring"
@@ -18,7 +19,9 @@ func Get(project string) (string, error) {
 		return "", err
 	}
 	val, err := keyring.Get(namespace, project)
-	if err != nil {
+	if errors.Is(err, exec.ErrNotFound) {
+		return "", errors.New(ErrNotSupported)
+	} else if err != nil {
 		return "", errors.Errorf("failed to load credentials: %w", err)
 	}
 	return val, nil
@@ -29,7 +32,9 @@ func Set(project, password string) error {
 	if err := assertKeyringSupported(); err != nil {
 		return err
 	}
-	if err := keyring.Set(namespace, project, password); err != nil {
+	if err := keyring.Set(namespace, project, password); errors.Is(err, exec.ErrNotFound) {
+		return errors.New(ErrNotSupported)
+	} else if err != nil {
 		return errors.Errorf("failed to set credentials: %w", err)
 	}
 	return nil
@@ -40,7 +45,9 @@ func Delete(project string) error {
 	if err := assertKeyringSupported(); err != nil {
 		return err
 	}
-	if err := keyring.Delete(namespace, project); err != nil {
+	if err := keyring.Delete(namespace, project); errors.Is(err, exec.ErrNotFound) {
+		return errors.New(ErrNotSupported)
+	} else if err != nil {
 		return errors.Errorf("failed to delete credentials: %w", err)
 	}
 	return nil
