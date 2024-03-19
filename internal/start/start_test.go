43b07dbe7b7d86d78f0def5a2c5b7d45ac45b7b7
Ziinc <Ziinc@users.noreply.github.com>
2024-03-19 12:20:20 +0800

    fix: enable analytics by default (#1919)
    * fix: enable analytics by default
    
    * chore: add vector in mock docker start
    
    * chore(deps): bump akhilerm/tag-push-action from 2.1.0 to 2.2.0 (#2054)
    
    Bumps [akhilerm/tag-push-action](https://github.com/akhilerm/tag-push-action) from 2.1.0 to 2.2.0.
    - [Release notes](https://github.com/akhilerm/tag-push-action/releases)
    - [Commits](https://github.com/akhilerm/tag-push-action/compare/v2.1.0...v2.2.0)
    
    ---
    updated-dependencies:
    - dependency-name: akhilerm/tag-push-action
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore: disable integration tests
    
    * chore: update unit tests
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Han Qiao <sweatybridge@gmail.com>
    Co-authored-by: Qiao Han <qiao@supabase.io>

--- internal/start/start_test.go
+++ internal/start/start_test.go
@@ -85,6 +85,7 @@ func TestStartCommand(t *testing.T) {
 
 func TestDatabaseStart(t *testing.T) {
 	t.Run("starts database locally", func(t *testing.T) {
+		utils.Config.Analytics.Enabled = false
 		// Setup in-memory fs
 		fsys := afero.NewMemMapFs()
 		// Setup mock docker
