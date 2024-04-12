446a8d66bcaafa8a907ed5e926c59b1eb6f6d5ea
Qiao Han <qiao@supabase.io>
2024-04-13 00:23:09 +0800

    fix: ignore managed schemas when reporting seq and idx scans

--- internal/inspect/queries.go
+++ internal/inspect/queries.go
@@ -109,22 +109,23 @@ AND c.relkind = 'i'
 GROUP BY c.relname
 ORDER BY sum(c.relpages) DESC`
 
-const INDEX_USAGE_QUERY = `SELECT relname,
-CASE
-  WHEN idx_scan IS NULL THEN 'Insufficient data'
-  WHEN idx_scan = 0 THEN 'Insufficient data'
-  ELSE (100 * idx_scan / (seq_scan + idx_scan))::text
-END percent_of_times_index_used,
-n_live_tup rows_in_table
-FROM
-pg_stat_user_tables
+const INDEX_USAGE_QUERY = `SELECT
+  relname,
+  CASE
+    WHEN idx_scan IS NULL THEN 'Insufficient data'
+    WHEN idx_scan = 0 THEN 'Insufficient data'
+    ELSE ROUND(100.0 * idx_scan / (seq_scan + idx_scan), 1) || '%'
+  END percent_of_times_index_used,
+  n_live_tup rows_in_table
+FROM pg_stat_user_tables
+WHERE NOT schemaname LIKE ANY($1)
 ORDER BY
-CASE
-  WHEN idx_scan is null then 1
-  WHEN idx_scan = 0 then 1
-  ELSE 0
-END,
-n_live_tup DESC`
+  CASE
+    WHEN idx_scan is null then 1
+    WHEN idx_scan = 0 then 1
+    ELSE 0
+  END,
+  n_live_tup DESC`
 
 const LOCKS_QUERY = `SELECT
 pg_stat_activity.pid,
@@ -191,10 +192,11 @@ FROM
 pg_roles
 ORDER BY 2 DESC`
 
-const SEQ_SCANS_QUERY = `SELECT relname AS name,
-seq_scan as count
-FROM
-pg_stat_user_tables
+const SEQ_SCANS_QUERY = `SELECT
+  relname AS name,
+  seq_scan as count
+FROM pg_stat_user_tables
+WHERE NOT schemaname LIKE ANY($1)
 ORDER BY seq_scan DESC`
 
 const TABLE_INDEX_SIZE_QUERY = `SELECT c.relname AS table,
