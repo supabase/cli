2b59575f8b8949e473b1d7a5419c1c4b0e8b8857
fenos <fabri.feno@gmail.com>
2024-04-15 15:30:42 +0100

    storage: fix cp across buckets on file driver

--- internal/utils/misc.go
+++ internal/utils/misc.go
@@ -42,7 +42,7 @@ const (
 	PgProveImage     = "supabase/pg_prove:3.36"
 	GotrueImage      = "supabase/gotrue:v2.145.0"
 	RealtimeImage    = "supabase/realtime:v2.27.5"
-	StorageImage     = "supabase/storage-api:v1.0.5"
+	StorageImage     = "supabase/storage-api:v1.0.6"
 	LogflareImage    = "supabase/logflare:1.4.0"
 	// Should be kept in-sync with EdgeRuntimeImage
 	DenoVersion = "1.30.3"
