e78be9bcc0537b5ea6c432496233aa34751a7d7f
Qiao Han <qiao@supabase.io>
2024-07-07 16:30:36 +0800

    feat: expose config as public package

---
+++ pkg/config/config_test.go
@@ -0,0 +1,214 @@
+package config
+
+import (
+	"bytes"
+	_ "embed"
+	"strings"
+	"testing"
+	fs "testing/fstest"
+
+	"github.com/BurntSushi/toml"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+//go:embed templates/init_config.test.toml
+var testInitConfigEmbed []byte
+
+func TestConfigParsing(t *testing.T) {
+	t.Run("classic config file", func(t *testing.T) {
+		config := NewConfig()
+		// Run test
+		var buf bytes.Buffer
+		require.NoError(t, config.Eject(&buf))
+		file := fs.MapFile{Data: buf.Bytes()}
+		fsys := fs.MapFS{"config.toml": &file}
+		// Check error
+		assert.NoError(t, config.Load("config.toml", fsys))
+	})
+
+	t.Run("config file with environment variables", func(t *testing.T) {
+		config := NewConfig()
+		// Setup in-memory fs
+		fsys := fs.MapFS{
+			"supabase/config.toml":           &fs.MapFile{Data: testInitConfigEmbed},
+			"supabase/templates/invite.html": &fs.MapFile{},
+		}
+		// Run test
+		t.Setenv("TWILIO_AUTH_TOKEN", "token")
+		t.Setenv("AZURE_CLIENT_ID", "hello")
+		t.Setenv("AZURE_SECRET", "this is cool")
+		t.Setenv("AUTH_SEND_SMS_SECRETS", "v1,whsec_aWxpa2VzdXBhYmFzZXZlcnltdWNoYW5kaWhvcGV5b3Vkb3Rvbw==")
+		t.Setenv("SENDGRID_API_KEY", "sendgrid")
+		assert.NoError(t, config.Load("", fsys))
+		// Check error
+		assert.Equal(t, "hello", config.Auth.External["azure"].ClientId)
+		assert.Equal(t, "this is cool", config.Auth.External["azure"].Secret)
+	})
+
+	t.Run("config file with environment variables fails when unset", func(t *testing.T) {
+		config := NewConfig()
+		// Setup in-memory fs
+		fsys := fs.MapFS{
+			"supabase/config.toml": &fs.MapFile{Data: testInitConfigEmbed},
+		}
+		// Run test
+		assert.Error(t, config.Load("", fsys))
+	})
+}
+
+func TestFileSizeLimitConfigParsing(t *testing.T) {
+	t.Run("test file size limit parsing number", func(t *testing.T) {
+		var testConfig config
+		_, err := toml.Decode(`
+		[storage]
+		file_size_limit = 5000000
+		`, &testConfig)
+		if assert.NoError(t, err) {
+			assert.Equal(t, sizeInBytes(5000000), testConfig.Storage.FileSizeLimit)
+		}
+	})
+
+	t.Run("test file size limit parsing bytes unit", func(t *testing.T) {
+		var testConfig config
+		_, err := toml.Decode(`
+		[storage]
+		file_size_limit = "5MB"
+		`, &testConfig)
+		if assert.NoError(t, err) {
+			assert.Equal(t, sizeInBytes(5242880), testConfig.Storage.FileSizeLimit)
+		}
+	})
+
+	t.Run("test file size limit parsing binary bytes unit", func(t *testing.T) {
+		var testConfig config
+		_, err := toml.Decode(`
+		[storage]
+		file_size_limit = "5MiB"
+		`, &testConfig)
+		if assert.NoError(t, err) {
+			assert.Equal(t, sizeInBytes(5242880), testConfig.Storage.FileSizeLimit)
+		}
+	})
+
+	t.Run("test file size limit parsing string number", func(t *testing.T) {
+		var testConfig config
+		_, err := toml.Decode(`
+		[storage]
+		file_size_limit = "5000000"
+		`, &testConfig)
+		if assert.NoError(t, err) {
+			assert.Equal(t, sizeInBytes(5000000), testConfig.Storage.FileSizeLimit)
+		}
+	})
+
+	t.Run("test file size limit parsing bad datatype", func(t *testing.T) {
+		var testConfig config
+		_, err := toml.Decode(`
+		[storage]
+		file_size_limit = []
+		`, &testConfig)
+		assert.Error(t, err)
+		assert.Equal(t, sizeInBytes(0), testConfig.Storage.FileSizeLimit)
+	})
+
+	t.Run("test file size limit parsing bad string data", func(t *testing.T) {
+		var testConfig config
+		_, err := toml.Decode(`
+		[storage]
+		file_size_limit = "foobar"
+		`, &testConfig)
+		assert.Error(t, err)
+		assert.Equal(t, sizeInBytes(0), testConfig.Storage.FileSizeLimit)
+	})
+}
+
+func TestSanitizeProjectI(t *testing.T) {
+	// Preserves valid consecutive characters
+	assert.Equal(t, "abc", sanitizeProjectId("abc"))
+	assert.Equal(t, "a..b_c", sanitizeProjectId("a..b_c"))
+	// Removes leading special characters
+	assert.Equal(t, "abc", sanitizeProjectId("_abc"))
+	assert.Equal(t, "abc", sanitizeProjectId("_@abc"))
+	// Replaces consecutive invalid characters with a single _
+	assert.Equal(t, "a_bc-", sanitizeProjectId("a@@bc-"))
+	// Truncates to less than 40 characters
+	sanitized := strings.Repeat("a", maxProjectIdLength)
+	assert.Equal(t, sanitized, sanitizeProjectId(sanitized+"bb"))
+}
+
+const (
+	defaultAnonKey        = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
+	defaultServiceRoleKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU"
+)
+
+func TestSigningJWT(t *testing.T) {
+	t.Run("signs default anon key", func(t *testing.T) {
+		anonToken := CustomClaims{Role: "anon"}.NewToken()
+		signed, err := anonToken.SignedString([]byte(defaultJwtSecret))
+		assert.NoError(t, err)
+		assert.Equal(t, defaultAnonKey, signed)
+	})
+
+	t.Run("signs default service_role key", func(t *testing.T) {
+		anonToken := CustomClaims{Role: "service_role"}.NewToken()
+		signed, err := anonToken.SignedString([]byte(defaultJwtSecret))
+		assert.NoError(t, err)
+		assert.Equal(t, defaultServiceRoleKey, signed)
+	})
+}
+
+func TestValidateHookURI(t *testing.T) {
+	tests := []struct {
+		name      string
+		uri       string
+		hookName  string
+		shouldErr bool
+		errorMsg  string
+	}{
+		{
+			name:      "valid http URL",
+			uri:       "http://example.com",
+			hookName:  "testHook",
+			shouldErr: false,
+		},
+		{
+			name:      "valid https URL",
+			uri:       "https://example.com",
+			hookName:  "testHook",
+			shouldErr: false,
+		},
+		{
+			name:      "valid pg-functions URI",
+			uri:       "pg-functions://functionName",
+			hookName:  "pgHook",
+			shouldErr: false,
+		},
+		{
+			name:      "invalid URI with unsupported scheme",
+			uri:       "ftp://example.com",
+			hookName:  "malformedHook",
+			shouldErr: true,
+			errorMsg:  "Invalid HTTP hook config: auth.hook.malformedHook should be a Postgres function URI, or a HTTP or HTTPS URL",
+		},
+		{
+			name:      "invalid URI with parsing error",
+			uri:       "http://a b.com",
+			hookName:  "errorHook",
+			shouldErr: true,
+			errorMsg:  "failed to parse template url: parse \"http://a b.com\": invalid character \" \" in host name",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := validateHookURI(tt.uri, tt.hookName)
+			if tt.shouldErr {
+				assert.Error(t, err, "Expected an error for %v", tt.name)
+				assert.EqualError(t, err, tt.errorMsg, "Expected error message does not match for %v", tt.name)
+			} else {
+				assert.NoError(t, err, "Expected no error for %v", tt.name)
+			}
+		})
+	}
+}
---
+++ pkg/config/utils.go
@@ -0,0 +1,98 @@
+package config
+
+import (
+	"path/filepath"
+	"strings"
+)
+
+type pathBuilder struct {
+	SupabaseDirPath       string
+	ConfigPath            string
+	GitIgnorePath         string
+	TempDir               string
+	ImportMapsDir         string
+	ProjectRefPath        string
+	PoolerUrlPath         string
+	PostgresVersionPath   string
+	GotrueVersionPath     string
+	RestVersionPath       string
+	StorageVersionPath    string
+	StudioVersionPath     string
+	PgmetaVersionPath     string
+	PoolerVersionPath     string
+	RealtimeVersionPath   string
+	CliVersionPath        string
+	CurrBranchPath        string
+	SchemasDir            string
+	MigrationsDir         string
+	FunctionsDir          string
+	FallbackImportMapPath string
+	FallbackEnvFilePath   string
+	DbTestsDir            string
+	SeedDataPath          string
+	CustomRolesPath       string
+}
+
+func NewPathBuilder(configPath string) pathBuilder {
+	if filepath.Base(configPath) == "." {
+		configPath = filepath.Join("supabase", "config.toml")
+	}
+	// TODO: make base path configurable from toml
+	base := filepath.Dir(configPath)
+	return pathBuilder{
+		SupabaseDirPath:       base,
+		ConfigPath:            configPath,
+		GitIgnorePath:         filepath.Join(base, ".gitignore"),
+		TempDir:               filepath.Join(base, ".temp"),
+		ImportMapsDir:         filepath.Join(base, ".temp", "import_maps"),
+		ProjectRefPath:        filepath.Join(base, ".temp", "project-ref"),
+		PoolerUrlPath:         filepath.Join(base, ".temp", "pooler-url"),
+		PostgresVersionPath:   filepath.Join(base, ".temp", "postgres-version"),
+		GotrueVersionPath:     filepath.Join(base, ".temp", "gotrue-version"),
+		RestVersionPath:       filepath.Join(base, ".temp", "rest-version"),
+		StorageVersionPath:    filepath.Join(base, ".temp", "storage-version"),
+		StudioVersionPath:     filepath.Join(base, ".temp", "studio-version"),
+		PgmetaVersionPath:     filepath.Join(base, ".temp", "pgmeta-version"),
+		PoolerVersionPath:     filepath.Join(base, ".temp", "pooler-version"),
+		RealtimeVersionPath:   filepath.Join(base, ".temp", "realtime-version"),
+		CliVersionPath:        filepath.Join(base, ".temp", "cli-latest"),
+		CurrBranchPath:        filepath.Join(base, ".branches", "_current_branch"),
+		SchemasDir:            filepath.Join(base, "schemas"),
+		MigrationsDir:         filepath.Join(base, "migrations"),
+		FunctionsDir:          filepath.Join(base, "functions"),
+		FallbackImportMapPath: filepath.Join(base, "functions", "import_map.json"),
+		FallbackEnvFilePath:   filepath.Join(base, "functions", ".env"),
+		DbTestsDir:            filepath.Join(base, "tests"),
+		SeedDataPath:          filepath.Join(base, "seed.sql"),
+		CustomRolesPath:       filepath.Join(base, "roles.sql"),
+	}
+}
+
+func ptr[T any](v T) *T {
+	return &v
+}
+
+func sliceContains[T comparable](s []T, e T) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func removeDuplicates(slice []string) (result []string) {
+	set := make(map[string]struct{})
+	for _, item := range slice {
+		if _, exists := set[item]; !exists {
+			set[item] = struct{}{}
+			result = append(result, item)
+		}
+	}
+	return result
+}
+
+func replaceImageTag(image string, tag string) string {
+	index := strings.IndexByte(image, ':')
+	return image[:index+1] + strings.TrimSpace(tag)
+}
