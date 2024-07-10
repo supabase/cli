278c8b8ecfebd09a80e29910af6f87383e10cc5c
Qiao Han <qiao@supabase.io>
2024-07-10 18:11:02 +0800

    fix: pass in project ref from seed command

--- internal/seed/buckets/buckets.go
+++ internal/seed/buckets/buckets.go
@@ -6,22 +6,24 @@ import (
 
 	"github.com/supabase/cli/internal/storage/client"
 	"github.com/supabase/cli/internal/utils"
-	"github.com/supabase/cli/internal/utils/flags"
 )
 
-func Run(ctx context.Context, console *utils.Console) error {
-	api, err := client.NewStorageAPI(ctx, flags.ProjectRef)
+func Run(ctx context.Context, projectRef string, console *utils.Console) error {
+	api, err := client.NewStorageAPI(ctx, projectRef)
 	if err != nil {
 		return err
 	}
 	config := utils.Config.Storage.GetBucketConfig()
-	api.UpsertBuckets(ctx, config, func(bucketId string) bool {
+	if console == nil {
+		return api.UpsertBuckets(ctx, config)
+	}
+	filter := func(bucketId string) bool {
 		label := fmt.Sprintf("Bucket %s already exists. Do you want to overwrite its properties?", utils.Bold(bucketId))
 		shouldOverwrite, err := console.PromptYesNo(ctx, label, true)
 		if err != nil {
 			fmt.Fprintln(utils.GetDebugLogger(), err)
 		}
 		return shouldOverwrite
-	})
-	return nil
+	}
+	return api.UpsertBuckets(ctx, config, filter)
 }
