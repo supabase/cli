bef875fd0727c39c1b0bb85819066cbe2f2d53e3
Filipe Cabaço <filipecabaco@gmail.com>
2024-09-03 00:59:24 +0100

    fix(realtime): Bump up docker image to 2.30.34 (#2651)

--- pkg/config/constants.go
+++ pkg/config/constants.go
@@ -16,7 +16,7 @@ const (
 	vectorImage      = "timberio/vector:0.28.1-alpine"
 	supavisorImage   = "supabase/supavisor:1.1.56"
 	gotrueImage      = "supabase/gotrue:v2.158.1"
-	realtimeImage    = "supabase/realtime:v2.30.23"
+	realtimeImage    = "supabase/realtime:v2.30.34"
 	storageImage     = "supabase/storage-api:v1.10.1"
 	logflareImage    = "supabase/logflare:1.4.0"
 	// Append to JobImages when adding new dependencies below
