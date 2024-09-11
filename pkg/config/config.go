0b0d45fbb3d5b51feb25a9edd65a8c7127c62a32
avallete <andrew.valleteau@supabase.io>
2024-09-11 12:57:33 +0200

    chore: remove unused type

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -82,10 +82,6 @@ type CustomClaims struct {
 	jwt.RegisteredClaims
 }
 
-type DbSeed struct {
-	Enabled bool
-}
-
 const (
 	defaultJwtSecret = "super-secret-jwt-token-with-at-least-32-characters-long"
 	defaultJwtExpiry = 1983812996
