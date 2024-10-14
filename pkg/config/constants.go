6797495ea8ecd61fd05ab2c156c81eb152fee575
Ivan Vasilov <vasilov.ivan@gmail.com>
2024-10-14 13:46:49 +0200

    Update studio version to 20241014

--- pkg/config/constants.go
+++ pkg/config/constants.go
@@ -10,7 +10,7 @@ const (
 	inbucketImage    = "inbucket/inbucket:3.0.3"
 	postgrestImage   = "postgrest/postgrest:v12.2.0"
 	pgmetaImage      = "supabase/postgres-meta:v0.83.2"
-	studioImage      = "supabase/studio:20241014-353d054"
+	studioImage      = "supabase/studio:20241014-c083b3b"
 	imageProxyImage  = "darthsim/imgproxy:v3.8.0"
 	edgeRuntimeImage = "supabase/edge-runtime:v1.58.13"
 	vectorImage      = "timberio/vector:0.28.1-alpine"
