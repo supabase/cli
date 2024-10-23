063dcf6e32ead83bf653c7b0e2a8872df87a7675
avallete <andrew.valleteau@supabase.io>
2024-10-23 18:26:01 +0200

    feat(cli): allow local postgres.conf configuration
    Closes: #2611

--- pkg/config/db.go
+++ pkg/config/db.go
@@ -1,6 +1,10 @@
 package config
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
+
 	"github.com/google/go-cmp/cmp"
 	v1API "github.com/supabase/cli/pkg/api"
 	"github.com/supabase/cli/pkg/cast"
@@ -144,6 +148,63 @@ func (a *settings) fromRemoteConfig(remoteConfig v1API.PostgresConfigResponse) s
 	result.WalSenderTimeout = remoteConfig.WalSenderTimeout
 	result.WorkMem = remoteConfig.WorkMem
 	return result
+}
+
+func (a *settings) ToPostgresConfig() string {
+	// create a valid string to append to /etc/postgresql/postgresql.conf
+	var config strings.Builder
+
+	// Helper function to add a setting if it's not nil
+	addSetting := func(name string, value interface{}) {
+		if value != nil {
+			switch v := value.(type) {
+			case *string:
+				if v != nil {
+					fmt.Fprintf(&config, "%s = '%s'\n", name, *v)
+				}
+			case *int:
+				if v != nil {
+					fmt.Fprintf(&config, "%s = %d\n", name, *v)
+				}
+			case *SessionReplicationRole:
+				if v != nil {
+					fmt.Fprintf(&config, "%s = '%s'\n", name, string(*v))
+				}
+			default:
+				// For other types, only add if it's a non-nil pointer
+				if reflect.ValueOf(value).Kind() == reflect.Ptr && !reflect.ValueOf(value).IsNil() {
+					fmt.Fprintf(&config, "%s = %v\n", name, reflect.ValueOf(value).Elem().Interface())
+				}
+			}
+		}
+	}
+
+	// Add all the settings
+	addSetting("effective_cache_size", a.EffectiveCacheSize)
+	addSetting("logical_decoding_work_mem", a.LogicalDecodingWorkMem)
+	addSetting("maintenance_work_mem", a.MaintenanceWorkMem)
+	addSetting("max_connections", a.MaxConnections)
+	addSetting("max_locks_per_transaction", a.MaxLocksPerTransaction)
+	addSetting("max_parallel_maintenance_workers", a.MaxParallelMaintenanceWorkers)
+	addSetting("max_parallel_workers", a.MaxParallelWorkers)
+	addSetting("max_parallel_workers_per_gather", a.MaxParallelWorkersPerGather)
+	addSetting("max_replication_slots", a.MaxReplicationSlots)
+	addSetting("max_slot_wal_keep_size", a.MaxSlotWalKeepSize)
+	addSetting("max_standby_archive_delay", a.MaxStandbyArchiveDelay)
+	addSetting("max_standby_streaming_delay", a.MaxStandbyStreamingDelay)
+	addSetting("max_wal_senders", a.MaxWalSenders)
+	addSetting("max_wal_size", a.MaxWalSize)
+	addSetting("max_worker_processes", a.MaxWorkerProcesses)
+	if a.SessionReplicationRole != nil {
+		addSetting("session_replication_role", string(*a.SessionReplicationRole))
+	}
+	addSetting("shared_buffers", a.SharedBuffers)
+	addSetting("statement_timeout", a.StatementTimeout)
+	addSetting("wal_keep_size", a.WalKeepSize)
+	addSetting("wal_sender_timeout", a.WalSenderTimeout)
+	addSetting("work_mem", a.WorkMem)
+
+	return config.String()
 }
 
 func (a *settings) DiffWithRemote(remoteConfig v1API.PostgresConfigResponse) ([]byte, error) {
