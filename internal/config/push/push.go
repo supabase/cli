65d8be4ca1b10c3f89cfd67cd836836f965b3729
Han Qiao <sweatybridge@gmail.com>
2024-11-14 14:22:06 +0800

    fix: use base config when no remote is declared (#2882)

--- internal/config/push/push.go
+++ internal/config/push/push.go
@@ -15,8 +15,12 @@ func Run(ctx context.Context, ref string, fsys afero.Fs) error {
 		return err
 	}
 	client := config.NewConfigUpdater(*utils.GetSupabase())
-	fmt.Fprintln(os.Stderr, "Pushing config to project:", ref)
-	remote, _ := utils.Config.GetRemoteByProjectRef(ref)
+	remote, err := utils.Config.GetRemoteByProjectRef(ref)
+	if err != nil {
+		// Use base config when no remote is declared
+		remote.ProjectId = ref
+	}
+	fmt.Fprintln(os.Stderr, "Pushing config to project:", remote.ProjectId)
 	console := utils.NewConsole()
 	keep := func(name string) bool {
 		title := fmt.Sprintf("Do you want to push %s config to remote?", name)
