16a0332f2e4aa7d72143955bb304feb187406d9e
Qiao Han <qiao@supabase.io>
2024-11-15 10:50:18 +0800

    fix: account for remotes when resetting local db

--- internal/migration/apply/apply.go
+++ internal/migration/apply/apply.go
@@ -2,11 +2,14 @@ package apply
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"github.com/jackc/pgx/v4"
 	"github.com/spf13/afero"
 	"github.com/supabase/cli/internal/migration/list"
 	"github.com/supabase/cli/internal/utils"
+	"github.com/supabase/cli/internal/utils/flags"
 	"github.com/supabase/cli/pkg/migration"
 )
 
@@ -22,10 +25,12 @@ func MigrateAndSeed(ctx context.Context, version string, conn *pgx.Conn, fsys af
 }
 
 func applySeedFiles(ctx context.Context, conn *pgx.Conn, fsys afero.Fs) error {
-	if !utils.Config.Db.Seed.Enabled {
+	remote, _ := utils.Config.GetRemoteByProjectRef(flags.ProjectRef)
+	if !remote.Db.Seed.Enabled {
+		fmt.Fprintln(os.Stderr, "Skipping seed because it is disabled in config.toml for project:", remote.ProjectId)
 		return nil
 	}
-	seeds, err := migration.GetPendingSeeds(ctx, utils.Config.Db.Seed.SqlPaths, conn, afero.NewIOFS(fsys))
+	seeds, err := migration.GetPendingSeeds(ctx, remote.Db.Seed.SqlPaths, conn, afero.NewIOFS(fsys))
 	if err != nil {
 		return err
 	}
