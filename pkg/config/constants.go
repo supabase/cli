3640979fc2098174880357258ef84f6557f6ddb2
Filipe Cabaço <filipecabaco@gmail.com>
2024-12-18 12:44:25 +0000

    realtime: Bump image to 2.33.70

--- pkg/config/constants.go
+++ pkg/config/constants.go
@@ -16,7 +16,7 @@ const (
 	vectorImage      = "timberio/vector:0.28.1-alpine"
 	supavisorImage   = "supabase/supavisor:1.1.56"
 	gotrueImage      = "supabase/gotrue:v2.164.0"
-	realtimeImage    = "supabase/realtime:v2.33.58"
+	realtimeImage    = "supabase/realtime:v2.33.70"
 	storageImage     = "supabase/storage-api:v1.14.5"
 	logflareImage    = "supabase/logflare:1.4.0"
 	// Append to JobImages when adding new dependencies below
