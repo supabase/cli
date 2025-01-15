2d25788bc0cfac8d3dcae5d927772fc74d54851b
Jonathan Braat <jonathan.braat@live.com>
2025-01-15 13:22:51 +1300

    [ADD] tests for new migration to include repeatable flag

--- internal/migration/new/new_test.go
+++ internal/migration/new/new_test.go
@@ -12,14 +12,14 @@ import (
 )
 
 func TestNewCommand(t *testing.T) {
-	t.Run("creates new migration file", func(t *testing.T) {
+	t.Run("creates new common migration file", func(t *testing.T) {
 		// Setup in-memory fs
 		fsys := afero.NewMemMapFs()
 		// Setup empty stdin
 		stdin, err := fsys.Create("/dev/stdin")
 		require.NoError(t, err)
 		// Run test
-		assert.NoError(t, Run("test_migrate", stdin, fsys))
+		assert.NoError(t, Run(false, "test_migrate", stdin, fsys))
 		// Validate output
 		files, err := afero.ReadDir(fsys, utils.MigrationsDir)
 		assert.NoError(t, err)
@@ -27,7 +27,22 @@ func TestNewCommand(t *testing.T) {
 		assert.Regexp(t, `([0-9]{14})_test_migrate\.sql`, files[0].Name())
 	})
 
-	t.Run("streams content from pipe", func(t *testing.T) {
+	t.Run("creates new repeatable migration file", func(t *testing.T) {
+		// Setup in-memory fs
+		fsys := afero.NewMemMapFs()
+		// Setup empty stdin
+		stdin, err := fsys.Create("/dev/stdin")
+		require.NoError(t, err)
+		// Run test
+		assert.NoError(t, Run(true, "repeatable_test_migrate", stdin, fsys))
+		// Validate output
+		files, err := afero.ReadDir(fsys, utils.MigrationsDir)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(files))
+		assert.Regexp(t, `r_repeatable_test_migrate\.sql`, files[0].Name())
+	})
+
+	t.Run("streams content from pipe to common migration", func(t *testing.T) {
 		// Setup in-memory fs
 		fsys := afero.NewMemMapFs()
 		// Setup stdin
@@ -38,7 +53,29 @@ func TestNewCommand(t *testing.T) {
 		require.NoError(t, err)
 		require.NoError(t, w.Close())
 		// Run test
-		assert.NoError(t, Run("test_migrate", r, fsys))
+		assert.NoError(t, Run(false, "test_migrate", r, fsys))
+		// Validate output
+		files, err := afero.ReadDir(fsys, utils.MigrationsDir)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(files))
+		path := filepath.Join(utils.MigrationsDir, files[0].Name())
+		contents, err := afero.ReadFile(fsys, path)
+		assert.NoError(t, err)
+		assert.Equal(t, []byte(script), contents)
+	})
+
+	t.Run("streams content from pipe to repeatable migration", func(t *testing.T) {
+		// Setup in-memory fs
+		fsys := afero.NewMemMapFs()
+		// Setup stdin
+		r, w, err := os.Pipe()
+		require.NoError(t, err)
+		script := "create table pet;\ndrop table pet;\n"
+		_, err = w.WriteString(script)
+		require.NoError(t, err)
+		require.NoError(t, w.Close())
+		// Run test
+		assert.NoError(t, Run(true, "repeatable_test_migrate", r, fsys))
 		// Validate output
 		files, err := afero.ReadDir(fsys, utils.MigrationsDir)
 		assert.NoError(t, err)
@@ -56,7 +93,8 @@ func TestNewCommand(t *testing.T) {
 		stdin, err := fsys.Create("/dev/stdin")
 		require.NoError(t, err)
 		// Run test
-		assert.Error(t, Run("test_migrate", stdin, afero.NewReadOnlyFs(fsys)))
+		assert.Error(t, Run(false, "test_migrate", stdin, afero.NewReadOnlyFs(fsys)))
+		assert.Error(t, Run(true, "repeatable_test_migrate", stdin, afero.NewReadOnlyFs(fsys)))
 	})
 
 	t.Run("throws error on closed pipe", func(t *testing.T) {
@@ -67,6 +105,7 @@ func TestNewCommand(t *testing.T) {
 		require.NoError(t, err)
 		require.NoError(t, r.Close())
 		// Run test
-		assert.Error(t, Run("test_migrate", r, fsys))
+		assert.Error(t, Run(false, "test_migrate", r, fsys))
+		assert.Error(t, Run(true, "repeatable_test_migrate", r, fsys))
 	})
 }
