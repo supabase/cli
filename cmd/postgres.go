3f522278340482c68308a4cb5e099c83c5933663
Andrew Valleteau <avallete@users.noreply.github.com>
2025-01-22 10:23:25 +0900

    feat(postgrest-config): add delete command to cli for convenience (#3060)

--- cmd/postgres.go
+++ cmd/postgres.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
+	"github.com/supabase/cli/internal/postgresConfig/delete"
 	"github.com/supabase/cli/internal/postgresConfig/get"
 	"github.com/supabase/cli/internal/postgresConfig/update"
 	"github.com/supabase/cli/internal/utils/flags"
@@ -33,8 +34,18 @@ Custom configuration also overrides the optimizations generated based on the com
 		},
 	}
 
+	postgresConfigDeleteCmd = &cobra.Command{
+		Use:   "delete",
+		Short: "Delete specific Postgres database config overrides",
+		Long:  "Delete specific config overrides, reverting them to their default values.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return delete.Run(cmd.Context(), flags.ProjectRef, postgresConfigKeysToDelete, noRestart, afero.NewOsFs())
+		},
+	}
+
 	postgresConfigValues            []string
 	postgresConfigUpdateReplaceMode bool
+	postgresConfigKeysToDelete      []string
 	noRestart                       bool
 )
 
@@ -42,11 +53,16 @@ func init() {
 	postgresCmd.PersistentFlags().StringVar(&flags.ProjectRef, "project-ref", "", "Project ref of the Supabase project.")
 	postgresCmd.AddCommand(postgresConfigGetCmd)
 	postgresCmd.AddCommand(postgresConfigUpdateCmd)
+	postgresCmd.AddCommand(postgresConfigDeleteCmd)
 
 	updateFlags := postgresConfigUpdateCmd.Flags()
 	updateFlags.StringSliceVar(&postgresConfigValues, "config", []string{}, "Config overrides specified as a 'key=value' pair")
 	updateFlags.BoolVar(&postgresConfigUpdateReplaceMode, "replace-existing-overrides", false, "If true, replaces all existing overrides with the ones provided. If false (default), merges existing overrides with the ones provided.")
 	updateFlags.BoolVar(&noRestart, "no-restart", false, "Do not restart the database after updating config.")
+
+	deleteFlags := postgresConfigDeleteCmd.Flags()
+	deleteFlags.StringSliceVar(&postgresConfigKeysToDelete, "config", []string{}, "Config keys to delete (comma-separated)")
+	deleteFlags.BoolVar(&noRestart, "no-restart", false, "Do not restart the database after deleting config.")
 
 	rootCmd.AddCommand(postgresCmd)
 }
