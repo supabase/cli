5efb52f000ffa4a9b2bb1d4c9e9985d49f1dcca1
Qiao Han <qiao@supabase.io>
2025-02-27 18:14:37 +0800

    fix: accept both branch ID and name

--- internal/branches/list/list.go
+++ internal/branches/list/list.go
@@ -10,22 +10,19 @@ import (
 	"github.com/supabase/cli/internal/migration/list"
 	"github.com/supabase/cli/internal/utils"
 	"github.com/supabase/cli/internal/utils/flags"
+	"github.com/supabase/cli/pkg/api"
 )
 
 func Run(ctx context.Context, fsys afero.Fs) error {
-	resp, err := utils.GetSupabase().V1ListAllBranchesWithResponse(ctx, flags.ProjectRef)
+	branches, err := ListBranch(ctx, flags.ProjectRef)
 	if err != nil {
-		return errors.Errorf("failed to list preview branches: %w", err)
-	}
-
-	if resp.JSON200 == nil {
-		return errors.New("Unexpected error listing preview branches: " + string(resp.Body))
+		return err
 	}
 
 	table := `|ID|NAME|DEFAULT|GIT BRANCH|STATUS|CREATED AT (UTC)|UPDATED AT (UTC)|
 |-|-|-|-|-|-|-|
 `
-	for _, branch := range *resp.JSON200 {
+	for _, branch := range branches {
 		gitBranch := " "
 		if branch.GitBranch != nil {
 			gitBranch = *branch.GitBranch
@@ -43,4 +40,32 @@ func Run(ctx context.Context, fsys afero.Fs) error {
 	}
 
 	return list.RenderTable(table)
+}
+
+type BranchFilter func(api.BranchResponse) bool
+
+func ListBranch(ctx context.Context, ref string, filter ...BranchFilter) ([]api.BranchResponse, error) {
+	resp, err := utils.GetSupabase().V1ListAllBranchesWithResponse(ctx, ref)
+	if err != nil {
+		return nil, errors.Errorf("failed to list branch: %w", err)
+	} else if resp.JSON200 == nil {
+		return nil, errors.Errorf("unexpected list branch status %d: %s", resp.StatusCode(), string(resp.Body))
+	}
+	var result []api.BranchResponse
+OUTER:
+	for _, branch := range *resp.JSON200 {
+		for _, keep := range filter {
+			if !keep(branch) {
+				continue OUTER
+			}
+		}
+		result = append(result, branch)
+	}
+	return result, nil
+}
+
+func FilterByName(branchName string) BranchFilter {
+	return func(br api.BranchResponse) bool {
+		return br.Name == branchName
+	}
 }
