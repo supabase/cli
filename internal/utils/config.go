f236fd37517ca0e0c923f5d486857207c4042a06
avallete <andrew.valleteau@supabase.io>
2025-04-03 14:46:20 +0200

    chore: nitpick

--- internal/utils/config.go
+++ internal/utils/config.go
@@ -130,7 +130,11 @@ func InitConfig(params InitParams, fsys afero.Fs) error {
 	c := config.NewConfig()
 	c.ProjectId = params.ProjectId
 	if params.UseOrioleDB {
-		c.Experimental.OrioleDBVersion = c.Db.MajorVersion > 15 ? "17.0.1.57" : "15.1.0.150"
+		if c.Db.MajorVersion > 15 {
+			c.Experimental.OrioleDBVersion = "17.0.1.57"
+		} else {
+			c.Experimental.OrioleDBVersion = "15.1.0.150"
+		}
 	}
 	// Create config file
 	if err := MkdirIfNotExistFS(fsys, SupabaseDirPath); err != nil {
