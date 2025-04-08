2d3bd368a46473940f468933868c0579e5f36e81
Andrew Valleteau <avallete@users.noreply.github.com>
2025-04-08 11:18:32 +0200

    feat(cli): setup postgres17 for local development (#3387)
    * feat(cli): setup postgres17 for local development
    
    * chore: use v17 by default
    
    * chore: nitpick
    
    * chore: apply pr comments
    
    * chore: fix lint
    
    * chore: set default image based on major version
    
    * chore: use current db image for dump
    
    ---------
    
    Co-authored-by: Qiao Han <qiao@supabase.io>

--- tools/selfhost/main.go
+++ tools/selfhost/main.go
@@ -61,7 +61,7 @@ func updateSelfHosted(ctx context.Context, branch string) error {
 }
 
 func getStableVersions() map[string]string {
-	images := append(config.Images.Services(), config.Images.Pg15)
+	images := append(config.Images.Services(), config.Images.Pg)
 	result := make(map[string]string, len(images))
 	for _, img := range images {
 		parts := strings.Split(img, ":")
