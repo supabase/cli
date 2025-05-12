4c8824433ac9769ea669b7097f7e7d6e73a43ca2
Chris Gwilliams <517923+encima@users.noreply.github.com>
2025-05-12 16:20:44 +0300

    update tests for all queries in inspect dir

--- internal/inspect/report_test.go
+++ internal/inspect/report_test.go
@@ -3,21 +3,12 @@ package inspect
 import (
 	"context"
 	"fmt"
+	"io/fs"
 	"testing"
 
 	"github.com/jackc/pgconn"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
-	"github.com/supabase/cli/internal/inspect/bloat"
-	"github.com/supabase/cli/internal/inspect/blocking"
-	"github.com/supabase/cli/internal/inspect/calls"
-	"github.com/supabase/cli/internal/inspect/db_stats"
-	"github.com/supabase/cli/internal/inspect/index_stats"
-	"github.com/supabase/cli/internal/inspect/locks"
-	"github.com/supabase/cli/internal/inspect/long_running_queries"
-	"github.com/supabase/cli/internal/inspect/outliers"
-	"github.com/supabase/cli/internal/inspect/replication_slots"
-	"github.com/supabase/cli/internal/inspect/vacuum_stats"
 	"github.com/supabase/cli/pkg/pgtest"
 )
 
@@ -31,38 +22,32 @@ var dbConfig = pgconn.Config{
 
 func TestReportCommand(t *testing.T) {
 	t.Run("runs all queries", func(t *testing.T) {
-		// Setup in-memory fs
 		fsys := afero.NewMemMapFs()
-		// Setup mock postgres
 		conn := pgtest.NewConn()
 		defer conn.Close(t)
-		conn.Query(wrapQuery(bloat.BloatQuery)).
-			Reply("COPY 0").
-			Query(wrapQuery(blocking.BlockingQuery)).
-			Reply("COPY 0").
-			Query(wrapQuery(calls.CallsQuery)).
-			Reply("COPY 0").
-			Query(wrapQuery(locks.LocksQuery)).
-			Reply("COPY 0").
-			Query(wrapQuery(long_running_queries.LongRunningQueriesQuery)).
-			Reply("COPY 0").
-			Query(wrapQuery(outliers.OutliersQuery)).
-			Reply("COPY 0").
-			Query(wrapQuery(replication_slots.ReplicationSlotsQuery)).
-			Reply("COPY 0").
-			Query(wrapQuery(db_stats.DBStatsQuery)).
-			Reply("COPY 0").
-			Query(wrapQuery(index_stats.IndexStatsQuery)).
-			Reply("COPY 0").
-			Query(wrapQuery(vacuum_stats.VacuumStatsQuery)).
-			Reply("COPY 0")
-		// Run test
-		err := Report(context.Background(), ".", dbConfig, fsys, conn.Intercept)
-		// Check error
+		// Iterate over all embedded SQL files
+		var sqlCount int
+		err := fs.WalkDir(queries, ".", func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+			if d.IsDir() {
+				return nil
+			}
+			data, err := queries.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			conn.Query(wrapQuery(string(data))).Reply("COPY 0")
+			sqlCount++
+			return nil
+		})
 		assert.NoError(t, err)
-		matches, err := afero.Glob(fsys, "*.csv")
+		err = Report(context.Background(), ".", dbConfig, fsys, conn.Intercept)
 		assert.NoError(t, err)
-		assert.Len(t, matches, 20)
+		matches, err := afero.Glob(fsys, "*/*.csv")
+		assert.NoError(t, err)
+		assert.Len(t, matches, sqlCount)
 	})
 }
 
