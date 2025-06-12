365129327ff5774390dc69f8de858d6505c81fd2
avallete <andrew.valleteau@supabase.io>
2025-06-12 12:26:48 +0200

    chore(logger): use slog for consistent logging

--- internal/utils/logger.go
+++ internal/utils/logger.go
@@ -1,12 +1,65 @@
 package utils
 
 import (
+	"context"
 	"fmt"
 	"io"
+	"log/slog"
 	"os"
 
 	"github.com/spf13/viper"
 )
+
+var logger *slog.Logger
+
+func init() {
+	// Custom handler for simple colored output
+	handler := &simpleHandler{output: os.Stderr}
+	logger = slog.New(handler)
+}
+
+// simpleHandler implements slog.Handler with simple colored output
+type simpleHandler struct {
+	output io.Writer
+}
+
+func (h *simpleHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return true
+}
+
+func (h *simpleHandler) Handle(ctx context.Context, record slog.Record) error {
+	var prefix string
+	var colorFunc func(string) string
+
+	switch record.Level {
+	case slog.LevelDebug:
+		prefix = "DEBUG:"
+		colorFunc = Aqua
+	case slog.LevelInfo:
+		prefix = "INFO:"
+		colorFunc = Blue
+	case slog.LevelWarn:
+		prefix = "WARNING:"
+		colorFunc = Yellow
+	case slog.LevelError:
+		prefix = "ERROR:"
+		colorFunc = Red
+	default:
+		prefix = "LOG:"
+		colorFunc = func(s string) string { return s }
+	}
+
+	fmt.Fprintf(h.output, "%s %s\n", colorFunc(prefix), record.Message)
+	return nil
+}
+
+func (h *simpleHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return h
+}
+
+func (h *simpleHandler) WithGroup(name string) slog.Handler {
+	return h
+}
 
 // GetVerbosity returns the current verbosity level (0 = minimal, 1 = normal info, 2+ = more verbose)
 func GetVerbosity() int {
@@ -20,26 +73,36 @@ func GetDebugLogger() io.Writer {
 	return io.Discard
 }
 
-// TODO: refactor across all codebase for consistency
-// Info logs an info message to stderr if the current verbosity level is >= the required level
-// level 0: always shown (important info)
-// level 1: normal verbosity
-// level 2+: high verbosity
+// Log logs a plain message with no formatting, colors, or prefixes over stdout
+func Log(format string, args ...interface{}) {
+	message := fmt.Sprintf(format, args...)
+	fmt.Fprintf(os.Stdout, "%s", message)
+}
+
+// Info logs an info message if the current SUPABASE_VERBOSITY level is >= the required level
 func Info(level int, format string, args ...interface{}) {
 	if GetVerbosity() >= level {
-		message := fmt.Sprintf(format, args...)
-		fmt.Fprintf(os.Stderr, "%s %s\n", Blue("INFO:"), message)
+		logger.Info(fmt.Sprintf(format, args...))
 	}
 }
 
-// Debug logs a debug message to the debug logger (stderr when DEBUG=true, otherwise discarded)
+// Debug logs a debug message when SUPABASE_DEBUG=true
 func Debug(format string, args ...interface{}) {
-	message := fmt.Sprintf(format, args...)
-	fmt.Fprintf(GetDebugLogger(), "%s %s\n", Aqua("DEBUG:"), message)
+	if viper.GetBool("DEBUG") {
+		logger.Debug(fmt.Sprintf(format, args...))
+	}
 }
 
-// Warning logs a warning message to stderr with consistent formatting and styling
+// Warning logs a warning message
 func Warning(format string, args ...interface{}) {
-	message := fmt.Sprintf(format, args...)
-	fmt.Fprintf(os.Stderr, "%s %s\n", Yellow("WARNING:"), message)
+	logger.Warn(fmt.Sprintf(format, args...))
+}
+
+// Error logs an error message
+func Error(format string, args ...interface{}) {
+	logger.Error(fmt.Sprintf(format, args...))
+}
+
+func GetLogger() *slog.Logger {
+	return logger
 }
