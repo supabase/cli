66217834330b5e7a85bcf4c991b9765ef92f0f9a
Qiao Han <qiao@supabase.io>
2025-07-17 19:59:22 +0800

    fix: escape drop schema statements

--- pkg/migration/drop_test.go
+++ pkg/migration/drop_test.go
@@ -9,18 +9,12 @@ import (
 	"github.com/supabase/cli/pkg/pgtest"
 )
 
-var escapedSchemas = append(ManagedSchemas, "extensions", "public")
-
 func TestDropSchemas(t *testing.T) {
 	t.Run("resets remote database", func(t *testing.T) {
 		// Setup mock postgres
 		conn := pgtest.NewConn()
 		defer conn.Close(t)
-		conn.Query(ListSchemas, escapedSchemas).
-			Reply("SELECT 1", []interface{}{"private"}).
-			Query("DROP SCHEMA IF EXISTS private CASCADE").
-			Reply("DROP SCHEMA").
-			Query(DropObjects).
+		conn.Query(DropObjects).
 			Reply("INSERT 0")
 		// Run test
 		err := DropUserSchemas(context.Background(), conn.MockClient(t))
@@ -28,25 +22,11 @@ func TestDropSchemas(t *testing.T) {
 		assert.NoError(t, err)
 	})
 
-	t.Run("throws error on list schema failure", func(t *testing.T) {
-		// Setup mock postgres
-		conn := pgtest.NewConn()
-		defer conn.Close(t)
-		conn.Query(ListSchemas, escapedSchemas).
-			ReplyError(pgerrcode.InsufficientPrivilege, "permission denied for relation information_schema")
-		// Run test
-		err := DropUserSchemas(context.Background(), conn.MockClient(t))
-		// Check error
-		assert.ErrorContains(t, err, "ERROR: permission denied for relation information_schema (SQLSTATE 42501)")
-	})
-
 	t.Run("throws error on drop schema failure", func(t *testing.T) {
 		// Setup mock postgres
 		conn := pgtest.NewConn()
 		defer conn.Close(t)
-		conn.Query(ListSchemas, escapedSchemas).
-			Reply("SELECT 0").
-			Query(DropObjects).
+		conn.Query(DropObjects).
 			ReplyError(pgerrcode.InsufficientPrivilege, "permission denied for relation supabase_migrations")
 		// Run test
 		err := DropUserSchemas(context.Background(), conn.MockClient(t))
--- pkg/migration/queries/list.sql
+++ pkg/migration/queries/list.sql
@@ -3,11 +3,8 @@
 --  Supabase managed schemas
 select pn.nspname
 from pg_namespace pn
-left join pg_depend pd
-  on pd.objid = pn.oid
-join pg_roles r 
-  on pn.nspowner = r.oid
+left join pg_depend pd on pd.objid = pn.oid
 where pd.deptype is null
   and not pn.nspname like any($1)
-  and r.rolname != 'supabase_admin'
+  and pn.nspowner::regrole::text != 'supabase_admin'
 order by pn.nspname
