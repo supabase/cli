61b41fd278b741db06592dc75d707e00fa3669bc
Copple <10214025+kiwicopple@users.noreply.github.com>
2025-07-24 18:25:31 +0200

    chore: sync API types from infrastructure (#3914)
    Co-authored-by: avallete <8771783+avallete@users.noreply.github.com>

--- pkg/api/client.gen.go
+++ pkg/api/client.gen.go
@@ -3049,6 +3049,22 @@ func NewV1AuthorizeUserRequest(server string, params *V1AuthorizeUserParams) (*h
 
 		}
 
+		if params.Resource != nil {
+
+			if queryFrag, err := runtime.StyleParamWithLocation("form", true, "resource", runtime.ParamLocationQuery, *params.Resource); err != nil {
+				return nil, err
+			} else if parsed, err := url.ParseQuery(queryFrag); err != nil {
+				return nil, err
+			} else {
+				for k, v := range parsed {
+					for _, v2 := range v {
+						queryValues.Add(k, v2)
+					}
+				}
+			}
+
+		}
+
 		queryURL.RawQuery = queryValues.Encode()
 	}
 
