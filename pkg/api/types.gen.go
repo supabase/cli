4c97d28917da120655afab2c0da55084e569fac5
avallete <8771783+avallete@users.noreply.github.com>
2025-10-18 07:39:35 +0000

    chore: sync API types from infrastructure

--- pkg/api/types.gen.go
+++ pkg/api/types.gen.go
@@ -1369,6 +1369,30 @@ const (
 	SA V1GetAvailableRegionsParamsContinent = "SA"
 )
 
+// Defines values for V1GetAvailableRegionsParamsDesiredInstanceSize.
+const (
+	Large                    V1GetAvailableRegionsParamsDesiredInstanceSize = "large"
+	Medium                   V1GetAvailableRegionsParamsDesiredInstanceSize = "medium"
+	Micro                    V1GetAvailableRegionsParamsDesiredInstanceSize = "micro"
+	N12xlarge                V1GetAvailableRegionsParamsDesiredInstanceSize = "12xlarge"
+	N16xlarge                V1GetAvailableRegionsParamsDesiredInstanceSize = "16xlarge"
+	N24xlarge                V1GetAvailableRegionsParamsDesiredInstanceSize = "24xlarge"
+	N24xlargeHighMemory      V1GetAvailableRegionsParamsDesiredInstanceSize = "24xlarge_high_memory"
+	N24xlargeOptimizedCpu    V1GetAvailableRegionsParamsDesiredInstanceSize = "24xlarge_optimized_cpu"
+	N24xlargeOptimizedMemory V1GetAvailableRegionsParamsDesiredInstanceSize = "24xlarge_optimized_memory"
+	N2xlarge                 V1GetAvailableRegionsParamsDesiredInstanceSize = "2xlarge"
+	N48xlarge                V1GetAvailableRegionsParamsDesiredInstanceSize = "48xlarge"
+	N48xlargeHighMemory      V1GetAvailableRegionsParamsDesiredInstanceSize = "48xlarge_high_memory"
+	N48xlargeOptimizedCpu    V1GetAvailableRegionsParamsDesiredInstanceSize = "48xlarge_optimized_cpu"
+	N48xlargeOptimizedMemory V1GetAvailableRegionsParamsDesiredInstanceSize = "48xlarge_optimized_memory"
+	N4xlarge                 V1GetAvailableRegionsParamsDesiredInstanceSize = "4xlarge"
+	N8xlarge                 V1GetAvailableRegionsParamsDesiredInstanceSize = "8xlarge"
+	Nano                     V1GetAvailableRegionsParamsDesiredInstanceSize = "nano"
+	Pico                     V1GetAvailableRegionsParamsDesiredInstanceSize = "pico"
+	Small                    V1GetAvailableRegionsParamsDesiredInstanceSize = "small"
+	Xlarge                   V1GetAvailableRegionsParamsDesiredInstanceSize = "xlarge"
+)
+
 // Defines values for V1GetSecurityAdvisorsParamsLintType.
 const (
 	Sql V1GetSecurityAdvisorsParamsLintType = "sql"
@@ -4137,10 +4161,16 @@ type V1GetAvailableRegionsParams struct {
 
 	// Continent Continent code to determine regional recommendations: NA (North America), SA (South America), EU (Europe), AF (Africa), AS (Asia), OC (Oceania), AN (Antarctica)
 	Continent *V1GetAvailableRegionsParamsContinent `form:"continent,omitempty" json:"continent,omitempty"`
+
+	// DesiredInstanceSize Desired instance size
+	DesiredInstanceSize *V1GetAvailableRegionsParamsDesiredInstanceSize `form:"desired_instance_size,omitempty" json:"desired_instance_size,omitempty"`
 }
 
 // V1GetAvailableRegionsParamsContinent defines parameters for V1GetAvailableRegions.
 type V1GetAvailableRegionsParamsContinent string
+
+// V1GetAvailableRegionsParamsDesiredInstanceSize defines parameters for V1GetAvailableRegions.
+type V1GetAvailableRegionsParamsDesiredInstanceSize string
 
 // V1ListActionRunsParams defines parameters for V1ListActionRuns.
 type V1ListActionRunsParams struct {
