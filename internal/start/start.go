73a484e27e251d2e3c173b4ebddb5fc89081fdf3
Cemal Kilic <cemalkilic96@gmail.com>
2025-11-03 15:56:09 +0300

    fix: set jwt_issuer default during config loading

--- internal/start/start.go
+++ internal/start/start.go
@@ -464,11 +464,6 @@ EOF
 			formatMapForEnvConfig(utils.Config.Auth.Sms.TestOTP, &testOTP)
 		}
 
-		jwtIssuer := utils.GetApiUrl("/auth/v1")
-		if utils.Config.Auth.JwtIssuer != "" {
-			jwtIssuer = utils.Config.Auth.JwtIssuer
-		}
-
 		env := []string{
 			"API_EXTERNAL_URL=" + utils.Config.Api.ExternalUrl,
 
@@ -487,7 +482,7 @@ EOF
 			"GOTRUE_JWT_DEFAULT_GROUP_NAME=authenticated",
 			fmt.Sprintf("GOTRUE_JWT_EXP=%v", utils.Config.Auth.JwtExpiry),
 			"GOTRUE_JWT_SECRET=" + utils.Config.Auth.JwtSecret.Value,
-			"GOTRUE_JWT_ISSUER=" + jwtIssuer,
+			"GOTRUE_JWT_ISSUER=" + utils.Config.Auth.JwtIssuer,
 
 			fmt.Sprintf("GOTRUE_EXTERNAL_EMAIL_ENABLED=%v", utils.Config.Auth.Email.EnableSignup),
 			fmt.Sprintf("GOTRUE_MAILER_SECURE_EMAIL_CHANGE_ENABLED=%v", utils.Config.Auth.Email.DoubleConfirmChanges),
