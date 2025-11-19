959b93d244d6b7d2fc3657486796cebbf81e4858
Max <hello@maxmetcalfe.com>
2025-11-19 07:24:14 -0300

    feat: support append mode when updating network restrictions (#4420)
    * remove confusing log from restrictions get
    
    * improve update logic
    
    * update test
    
    * move append logic behind flag
    
    * remove trailing whitespace
    
    * fix lint
    
    * try another lint fix
    
    * update to use PATCH endpoint
    
    * fix lint
    
    * chore: minor refactor
    
    ---------
    
    Co-authored-by: Qiao Han <qiao@supabase.io>

--- internal/restrictions/update/update_test.go
+++ internal/restrictions/update/update_test.go
@@ -19,22 +19,52 @@ func TestUpdateRestrictionsCommand(t *testing.T) {
 	token := apitest.RandomAccessToken(t)
 	t.Setenv("SUPABASE_ACCESS_TOKEN", string(token))
 
-	t.Run("updates v4 and v6 CIDR", func(t *testing.T) {
+	t.Run("replaces v4 and v6 CIDR", func(t *testing.T) {
 		// Setup mock api
 		defer gock.OffAll()
+		expectedV4 := []string{"12.3.4.5/32", "1.2.3.1/24"}
+		expectedV6 := []string{"2001:db8:abcd:0012::0/64"}
 		gock.New(utils.DefaultApiHost).
 			Post("/v1/projects/" + projectRef + "/network-restrictions/apply").
 			MatchType("json").
 			JSON(api.NetworkRestrictionsRequest{
-				DbAllowedCidrs:   &[]string{"12.3.4.5/32", "1.2.3.1/24"},
-				DbAllowedCidrsV6: &[]string{"2001:db8:abcd:0012::0/64"},
+				DbAllowedCidrs:   &expectedV4,
+				DbAllowedCidrsV6: &expectedV6,
 			}).
 			Reply(http.StatusCreated).
 			JSON(api.NetworkRestrictionsResponse{
 				Status: api.NetworkRestrictionsResponseStatus("applied"),
 			})
 		// Run test
-		err := Run(context.Background(), projectRef, []string{"12.3.4.5/32", "2001:db8:abcd:0012::0/64", "1.2.3.1/24"}, false)
+		err := Run(context.Background(), projectRef, []string{"12.3.4.5/32", "2001:db8:abcd:0012::0/64", "1.2.3.1/24"}, false, false)
+		// Check error
+		assert.NoError(t, err)
+		assert.Empty(t, apitest.ListUnmatchedRequests())
+	})
+
+	t.Run("appends v4 and v6 CIDR", func(t *testing.T) {
+		// Setup mock api
+		defer gock.OffAll()
+		addV4 := []string{"12.3.4.5/32", "1.2.3.1/24"}
+		addV6 := []string{"2001:db8:abcd:0012::0/64"}
+		gock.New(utils.DefaultApiHost).
+			Patch("/v1/projects/" + projectRef + "/network-restrictions").
+			MatchType("json").
+			JSON(api.NetworkRestrictionsPatchRequest{
+				Add: &struct {
+					DbAllowedCidrs   *[]string `json:"dbAllowedCidrs,omitempty"`
+					DbAllowedCidrsV6 *[]string `json:"dbAllowedCidrsV6,omitempty"`
+				}{
+					DbAllowedCidrs:   &addV4,
+					DbAllowedCidrsV6: &addV6,
+				},
+			}).
+			Reply(http.StatusOK).
+			JSON(api.NetworkRestrictionsV2Response{
+				Status: api.NetworkRestrictionsV2ResponseStatus("applied"),
+			})
+		// Run test
+		err := Run(context.Background(), projectRef, []string{"12.3.4.5/32", "1.2.3.1/24", "2001:db8:abcd:0012::0/64"}, false, true)
 		// Check error
 		assert.NoError(t, err)
 		assert.Empty(t, apitest.ListUnmatchedRequests())
@@ -53,7 +83,7 @@ func TestUpdateRestrictionsCommand(t *testing.T) {
 			}).
 			ReplyError(errNetwork)
 		// Run test
-		err := Run(context.Background(), projectRef, []string{}, true)
+		err := Run(context.Background(), projectRef, []string{}, true, false)
 		// Check error
 		assert.ErrorIs(t, err, errNetwork)
 		assert.Empty(t, apitest.ListUnmatchedRequests())
@@ -71,7 +101,7 @@ func TestUpdateRestrictionsCommand(t *testing.T) {
 			}).
 			Reply(http.StatusServiceUnavailable)
 		// Run test
-		err := Run(context.Background(), projectRef, []string{}, true)
+		err := Run(context.Background(), projectRef, []string{}, true, false)
 		// Check error
 		assert.ErrorContains(t, err, "failed to apply network restrictions:")
 		assert.Empty(t, apitest.ListUnmatchedRequests())
@@ -99,7 +129,7 @@ func TestValidateCIDR(t *testing.T) {
 				Status: api.NetworkRestrictionsResponseStatus("applied"),
 			})
 		// Run test
-		err := Run(context.Background(), projectRef, []string{"10.0.0.0/8"}, true)
+		err := Run(context.Background(), projectRef, []string{"10.0.0.0/8"}, true, false)
 		// Check error
 		assert.NoError(t, err)
 		assert.Empty(t, apitest.ListUnmatchedRequests())
@@ -107,14 +137,14 @@ func TestValidateCIDR(t *testing.T) {
 
 	t.Run("throws error on private subnet", func(t *testing.T) {
 		// Run test
-		err := Run(context.Background(), projectRef, []string{"12.3.4.5/32", "10.0.0.0/8", "1.2.3.1/24"}, false)
+		err := Run(context.Background(), projectRef, []string{"12.3.4.5/32", "10.0.0.0/8", "1.2.3.1/24"}, false, false)
 		// Check error
 		assert.ErrorContains(t, err, "private IP provided: 10.0.0.0/8")
 	})
 
 	t.Run("throws error on invalid subnet", func(t *testing.T) {
 		// Run test
-		err := Run(context.Background(), projectRef, []string{"12.3.4.5", "10.0.0.0/8", "1.2.3.1/24"}, false)
+		err := Run(context.Background(), projectRef, []string{"12.3.4.5", "10.0.0.0/8", "1.2.3.1/24"}, false, false)
 		// Check error
 		assert.ErrorContains(t, err, "failed to parse IP: 12.3.4.5")
 	})
