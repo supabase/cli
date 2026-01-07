62919143042dd868de3f8040e26c955434b82b08
Han Qiao <sweatybridge@gmail.com>
2026-01-07 21:37:43 +0800

    chore: remove unused region method

--- cmd/projects.go
+++ cmd/projects.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"os"
-	"sort"
 
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
@@ -150,13 +149,4 @@ func init() {
 	projectsCmd.AddCommand(projectsListCmd)
 	projectsCmd.AddCommand(projectsApiKeysCmd)
 	rootCmd.AddCommand(projectsCmd)
-}
-
-func awsRegions() []string {
-	result := make([]string, len(utils.CurrentProfile.ProjectRegions))
-	for i, region := range utils.CurrentProfile.ProjectRegions {
-		result[i] = string(region)
-	}
-	sort.Strings(result)
-	return result
 }
