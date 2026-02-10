9fce3b775c152923f3385c3711f61a67bba3c926
Han Qiao <sweatybridge@gmail.com>
2026-02-10 17:17:35 +0800

    chore: unit tests for custom domains (#4835)
    * chore: unit tests for custom domains
    
    * chore: refactor status message printing

--- internal/hostnames/delete/delete.go
+++ internal/hostnames/delete/delete.go
@@ -3,6 +3,8 @@ package delete
 import (
 	"context"
 	"fmt"
+	"net/http"
+	"os"
 
 	"github.com/go-errors/errors"
 	"github.com/spf13/afero"
@@ -10,17 +12,12 @@ import (
 )
 
 func Run(ctx context.Context, projectRef string, fsys afero.Fs) error {
-	// 1. Sanity checks.
-	// 2. delete config
-	{
-		resp, err := utils.GetSupabase().V1DeleteHostnameConfigWithResponse(ctx, projectRef)
-		if err != nil {
-			return errors.Errorf("failed to delete custom hostname: %w", err)
-		}
-		if resp.StatusCode() != 200 {
-			return errors.New("failed to delete custom hostname config; received: " + resp.Status())
-		}
-		fmt.Println("Deleted custom hostname config successfully.")
-		return nil
+	resp, err := utils.GetSupabase().V1DeleteHostnameConfigWithResponse(ctx, projectRef)
+	if err != nil {
+		return errors.Errorf("failed to delete custom hostname: %w", err)
+	} else if resp.StatusCode() != http.StatusOK {
+		return errors.Errorf("unexpected delete hostname status %d: %s", resp.StatusCode(), string(resp.Body))
 	}
+	fmt.Fprintln(os.Stderr, "Deleted custom hostname config successfully.")
+	return nil
 }
