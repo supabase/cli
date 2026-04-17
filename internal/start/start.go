bd043db5c71089e5449d644170bdc6ef02639299
kazuki <kazuki.hara.dw3@gmail.com>
2026-04-18 07:39:33 +0900

    fix: for parity with the docker desktop comparison

--- internal/start/start.go
+++ internal/start/start.go
@@ -381,7 +381,8 @@ EOF
 				return errors.Errorf("failed to parse default host: %w", err)
 			} else if strings.HasSuffix(parsed.Host, "/.docker/run/docker.sock") ||
 				strings.HasSuffix(parsed.Host, "/.docker/desktop/docker.sock") ||
-				strings.Contains(parsed.Host, "/.colima/") {
+				strings.HasSuffix(parsed.Host, "/.colima/default/docker.sock") ||
+				strings.HasSuffix(parsed.Host, "/.colima/docker.sock") {
 				// Docker Desktop and Colima will not mount rootless socket directly;
 				// instead, specify root socket to have it handled under the hood
 				binds = append(binds, fmt.Sprintf("%[1]s:%[1]s:ro", dindHost.Host))
