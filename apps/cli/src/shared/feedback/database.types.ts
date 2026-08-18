ac97fbae29d98fee80edab067893e388202a6964
Kanad Gupta <git@kanad.dev>
2026-08-18 13:33:14 -0700

    chore: more script changes, remove non-public schemas

--- apps/cli/src/shared/feedback/database.types.ts
+++ apps/cli/src/shared/feedback/database.types.ts
@@ -12,31 +12,6 @@ export type Database = {
   __InternalSupabase: {
     PostgrestVersion: "14.5"
   }
-  graphql_public: {
-    Tables: {
-      [_ in never]: never
-    }
-    Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      graphql: {
-        Args: {
-          extensions?: Json
-          operationName?: string
-          query?: string
-          variables?: Json
-        }
-        Returns: Json
-      }
-    }
-    Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
-  }
   public: {
     Tables: {
       feedback: {
@@ -980,9 +955,6 @@ export type CompositeTypes<
     : never
 
 export const Constants = {
-  graphql_public: {
-    Enums: {},
-  },
   public: {
     Enums: {
       feedback_vote: ["yes", "no"],
