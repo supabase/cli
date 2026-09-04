102bb9cd6496ee6d06d5d7d8f8f4a29c556dd100
Julien Goux <hi@jgoux.dev>
2026-09-04 19:38:29 +0200

    fix(stack): restore coverage provider

--- packages/stack/vitest.config.ts
+++ packages/stack/vitest.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     passWithNoTests: true,
+    coverage: {
+      enabled: false,
+      provider: "istanbul",
+      clean: false,
+      include: ["src/**/*.ts"],
+      reporter: ["text", "lcov"],
+      reportsDirectory: "coverage",
+    },
     projects: [
       {
         test: {
